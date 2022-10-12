d959a43c2a4786e2892d9b4f90b4f6609be2a0d6
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2022-10-12 12:27:07 -0400

    Added blog author info card and image functionality
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- pages/blog/author/[author].vue
+++ pages/blog/author/[author].vue
@@ -1,23 +1,34 @@
 <template>
   <div>
     <div> <Header /> </div>
-    <div v-if="isLoaded" class="relative py-16 bg-white dark:bg-dark-bg overflow-hidden h-full pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-      <div class="relative px-4 sm:px-6 lg:px-8">
-        <div class="text-lg prose prose-lg max-w-prose mx-auto ">
-          <h1>
-            <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 dark:text-MITRE-silver sm:text-4xl">
-              {{ authorName }}
-            </span>
-            <span class="block text-xl pt-10 text-center underline dark:text-MITRE-silver">
-              Recent Articles
-            </span>
-          </h1>
-          <!-- <span v-html="renderedContent" class="mt-8 text-xl mx-auto leading-8 prose prose-sm dark:prose-invert"></span> -->
-          <!-- <div class="mt-8 mx-auto leading-8 text-center prose prose-sm lg:prose-lg dark:prose-invert prose-li:text-start hover:prose-headings:text-red-600" v-html="renderedContent" ></div> -->
-
+    <div class="relative py-16 bg-white dark:bg-dark-bg overflow-hidden min-h-screen h-full pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <template v-if="isLoaded">
+        <div class="relative px-4 sm:px-6 lg:px-32">
+          <div class="relative bg-blue-800 rounded-3xl">
+            <div class="absolute inset-0">
+              <!-- <img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100" alt="" /> -->
+              <div class="absolute inset-0 bg-blue-500 mix-blend-multiply opacity-70 rounded-3xl" aria-hidden="true" />
+            </div>
+            <div class="sm:flex justify-between items-center">
+              <div v-if="author.photo" class="relative order-2 sm:mr-8">
+                <img v-bind:src="author.photo.url" v-bind:alt="author.photo.name" class="max-w-full h-auto max-h-36 mx-auto pt-10 sm:py-0 align-middle" /> 
+              </div>
+              <div class="relative order-1 max-w-7xl ml-8 py-12 px-4 sm:py-12 sm:px-6 lg:px-1">
+                <h1 class="mt-2 block text-2xl sm:text-3xl leading-8 font-extrabold tracking-tight text-white">
+                  {{ authorName }} 
+                </h1>
+                <div class="mt-2 max-w-3xl text-lg text-dark-text pl-1"> {{author.company}} - {{author.title}} </div>
+                
+              </div>  
+            </div>
+            <div class="prose mt-2 ml-8 mr-8 pb-4 sm:justify-center max-w-3xl text-white dark:text-MITRE-silver" v-html="author.description" /> 
+          </div>
         </div>
-      </div>
-      <div class="mt-4 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+        <span class="block text-xl pt-12 text-center underline dark:text-dark-text">
+                Recent Articles
+        </span>  
+        <!-- <BlogPostCards graphql-query="author"/>  -->
+        <div class="mt-4 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
           <div v-for="post in posts" :key="post.title"
             class="flex flex-col rounded-lg shadow-xl border-2 hover:border-2 hover:border-blue-500 dark:hover:border-blue-500 dark:border-gray-600 overflow-hidden">
             <div
@@ -58,50 +69,77 @@
             </div>
           </div>
         </div>
+      </template>
+      <template v-else class="min-h-screen h-full ">
+        <h1 class="mt-2 block text-2xl sm:text-3xl leading-8 font-extrabold tracking-tight text-black dark:text-white">Loading...</h1> 
+      </template>
     </div>
-    <div v-else class="h-screen"> 
+    <!-- <div v-else class="h-screen"> 
       <p> Loading .... </p>
-    </div>
+    </div> -->
     <div> <Footer /> </div>
   </div>
 </template>
 
 <script>
-export default {
-  setup() {
+import { marked } from 'marked'
 
-  },
+export default {
   data() {
     return {
       posts: [],
       authorName: {},
+      author: {},
       isLoaded: false,
     };
   },
   mounted() {
     this.authorName = this.$route.query.id
     this.$nextTick(async () => {
-      await this.getData()
+      await this.getBlogData()
+      await this.getBlogAuthor()
+      this.author = this.author[0]
       this.isLoaded = true
     });
   },
   methods: {
-  async getData() {
-    console.log(this.$route.query)
-    this.posts = await useAsyncData('getBlogDataFromAuthor', () => GqlGetBlogDataFromAuthor({author: this.$route.query.id}))
-      .then(({ data }) => {
-        console.log(data._value.blogPosts.data)
-        return data._value.blogPosts.data.map((post) => ({
-            title: post.attributes.title,
-            description: post.attributes.description,
-            category: {name: post.attributes.category},
-            author: post.attributes.author,
-            date: post.attributes.date,
-            id: post.id,
-            readingTime: this.readingTime(post.attributes.content),
+    async getBlogData() {
+      console.log(this.$route.query)
+      this.posts = await useAsyncData('getBlogDataFromAuthor', () => GqlGetBlogDataFromAuthor({author: this.$route.query.id}))
+        .then(({ data }) => {
+          console.log(data._value.blogPosts.data)
+          return data._value.blogPosts.data.map((post) => ({
+              title: post.attributes.title,
+              description: post.attributes.description,
+              category: {name: post.attributes.category},
+              author: post.attributes.users_permissions_user.data.attributes.name,
+              date: post.attributes.date,
+              id: post.id,
+              readingTime: this.readingTime(post.attributes.content),
           }))
-          
-      });
+            
+        });
+    },
+    async getBlogAuthor(){
+      this.author = await useAsyncData('getBlogAuthor', () => GqlGetBlogAuthor({author: this.$route.query.id}))
+        .then(({data}) => {
+          console.log(data._value.usersPermissionsUsers.data)
+          return data._value.usersPermissionsUsers.data.map((author) => ({
+              name: author.attributes.name,
+              title: author.attributes.jobTitle,
+              company: author.attributes.company.replace('_', ' '),
+              description: marked(author.attributes.description),
+              photo:{
+                name: author.attributes.photo.data.attributes.name,
+                url: author.attributes.photo.data.attributes.url,
+              }, 
+              displayEmail: author.attributes.displayEmail,
+              linkedin: author.attributes.linkedIn,
+              twitter: author.attributes.twitter,
+              facebook: author.attributes.facebook,
+              instagram: author.attributes.instagram,
+          }))
+        })
     },
     readingTime(text) {
       const wpm = 225;
@@ -114,7 +152,6 @@ export default {
       str = str.replace(/[^\w\s-]/g, '')
       str = str.replace(/[\s_-]+/g, '-')
       str = str.replace(/^-+|-+$/g, '')
-      console.log(str)
       return str
     },
   }

34e7a092b0df330aa85928ae162ea4719e8a989d
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2022-10-24 12:04:23 -0400

    Updated blog author page styling
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- pages/blog/[author].vue
+++ pages/blog/[author].vue
@@ -4,43 +4,75 @@
     <div class="relative py-16 bg-white dark:bg-dark-bg overflow-hidden min-h-screen h-full pt-12 pb-20 px-4 sm:px-6 lg:pt-12 lg:pb-28 lg:px-8">
       <template v-if="isLoaded">
         <div class="relative px-4 sm:px-6 lg:px-32">
-          <div class="relative bg-blue-900 rounded-3xl">
-            <div class="absolute ">
-              <!-- <img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100" alt="" /> -->
-              <div class="absolute  bg-blue-500 mix-blend-multiply opacity-70 rounded-3xl" aria-hidden="true" />
-            </div>
-            <div class="sm:flex justify-between items-center">
-              <div v-if="author.photo" class="relative order-2 sm:mr-8">
-                <img v-bind:src="author.photo.url" v-bind:alt="author.photo.name" class="max-w-full h-auto max-h-28 sm:max-h-36 mx-auto pt-10 sm:py-6 align-middle" /> 
+          <article>
+            <!-- Profile header -->
+            <div>
+              <div>
+                <div class="h-16 w-full object-cover" />
               </div>
-              <div class="relative order-1 max-w-7xl ml-4 sm:ml-8 py-6 px-4 sm:py-4 sm:pt-0 sm:px-6 lg:px-1">
-                <h1 class="mt-2 block text-2xl sm:text-3xl leading-8 font-extrabold tracking-tight text-white">
-                  {{ authorName }} 
-                </h1>
-                <div class="mt-2 max-w-3xl text-lg text-dark-text pl-1"> {{author.company}} - {{author.title}} </div>
-              </div>  
+              <div class="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+                <div class="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+                  <div v-if="author.photo" class="flex">
+                    <img class="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32" :src="author.photo.url" v-bind:alt="author.photo.name" />
+                  </div>
+                  <div class="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                    <div class="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
+                      <h1 class="truncate text-2xl font-bold text-gray-900 dark:text-white">{{ author.name }}</h1>
+                    </div>
+                  </div>
+                </div>
+                <div class="mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden">
+                  <h1 class="truncate text-2xl font-bold text-gray-900 dark:text-white">{{ author.name }}</h1>
+                </div>
+              </div>
             </div>
-            <div class="prose mt-2 ml-8 mr-8 pb-4 sm:justify-center max-w-5xl text-sm sm:text-base tracking-wide sm:tracking-normal text-white dark:text-MITRE-silver" v-html="author.description" /> 
-            <div id="Social Medias" class="flex gap-2 pl-8 pb-5">
-              <a v-if="author.emailLink && author.displayEmail != null" v-bind:href="author.emailLink">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-8 h-8">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-                </svg>
-              </a>
-              <a v-if="author.twitter" v-bind:href="author.twitter">
-                <img src="~/assets/TwitterLogoWhite.png" class="max-w-full h-auto max-h-8" alt="Twitter Logo" />
-              </a>
-              <a v-if="author.facebook" v-bind:href="author.facebook">
-                <img src="~/assets/FacebookLogoWhite.png" class="max-w-full h-auto max-h-8" alt="Facebook Logo" />
-              </a>
-              <a v-if="author.instagram" v-bind:href="author.instagram">
-                <img src="~/assets/InstagramLogoWhite.png" class="max-w-full h-auto max-h-8" alt="Instagram Logo" />
-              </a>
-              <a v-if="author.linkedin" v-bind:href="author.linkedin">
-                <img src="~/assets/LinkedInLogoWhite.png" class="max-w-full h-auto max-h-8" alt="LinkedIn Logo" />
-              </a>
+
+            <!-- Description list -->
+            <div class="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
+              <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                  <div>
+                    <dt class="text-sm font-medium text-gray-500 dark:text-dark-text"> Title </dt>
+                    <dd class="sm:mt-1 text-sm text-gray-900 dark:text-dark-text">{{ author.fields.title }}</dd>
+                  </div>
+                  <div>
+                    <dt class="text-sm font-medium text-gray-500 dark:text-dark-text"> Company </dt>
+                    <dd class="sm:mt-1 text-sm text-gray-900 dark:text-dark-text">{{ author.fields.company }}</dd>
+                  </div>
+                <div class="sm:col-span-2">
+                  <dt class="text-sm font-medium text-gray-500 dark:text-dark-text">About</dt>
+                  <div class="prose mt-1 max-w-prose space-y-5 text-sm text-gray-900 dark:text-dark-text" v-html="author.description" />
+                  
+                </div>
+              </dl>
+              <div id="Social Medias" class="flex gap-2 pt-5 pb-5">
+                <a v-if="socialMediaLinks.displayEmail != null" v-bind:href="'mailto:' + socialMediaLinks.displayEmail">
+                  <MailIcon  text-gray-500 class="w-9 h-9 dark:text-dark-text" />
+                </a>
+                <a v-if="socialMediaLinks.twitter" v-bind:href="socialMediaLinks.twitter">
+                  <img src="~/assets/TwitterLogo.png" class="max-w-full h-auto max-h-8" alt="Twitter Logo" />
+                </a>
+                <a v-if="socialMediaLinks.facebook" v-bind:href="socialMediaLinks.facebook">
+                  <img src="~/assets/FacebookLogo.png" class="max-w-full h-auto max-h-8" alt="Facebook Logo" />
+                </a>
+                <a v-if="socialMediaLinks.instagram" v-bind:href="socialMediaLinks.instagram">
+                  <img src="~/assets/InstagramLogo.png" class="max-w-full h-auto max-h-8" alt="Instagram Logo" />
+                </a>
+                <a v-if="socialMediaLinks.github" v-bind:href="socialMediaLinks.github" class="dark:hidden">
+                  <img src="~/assets/GitHubLogo.png" class="max-w-full h-auto max-h-8" alt="GitHub Logo" />
+                </a>
+                <a v-if="socialMediaLinks.github" v-bind:href="socialMediaLinks.github" class="hidden dark:flex">
+                  <img src="~/assets/GitHubLogoWhite.png" class="max-w-full h-auto max-h-8" alt="GitHub Logo" />
+                </a>
+                <a v-if="socialMediaLinks.linkedin" v-bind:href="socialMediaLinks.linkedin">
+                  <img src="~/assets/LinkedInLogo.png" class="max-w-full h-auto max-h-8" alt="LinkedIn Logo" />
+                </a>
+                <a v-if="socialMediaLinks.other" v-bind:href="socialMediaLinks.other">
+                  <LinkIcon class="w-7 h-8 dark:text-dark-text" />
+                </a>
+              </div>
             </div>
-          </div>
+            <div class="border-b border-gray-200 pb-5" />
+          </article>
         </div>
         <span class="block text-xl pt-12 text-center underline font-bold dark:text-dark-text">
                 Recent Articles
@@ -71,7 +103,7 @@
                 </div>
                 <div class="ml-3">
                   <span class="text-sm font-medium text-gray-900 dark:text-dark-text ">
-                    <a :href="`/blog/author/${slugify(post.author)}?id=${post.author}`" class="hover:underline">
+                    <a :href="`/blog/${slugify(post.author)}?id=${post.author}`" class="hover:underline">
                       <p class="">{{post.author}}</p>  
                     </a>
                   </span>
@@ -101,14 +133,25 @@
 
 <script>
 import { marked } from 'marked'
+import {LinkIcon, MailIcon} from '@heroicons/vue/outline'
 
 export default {
+  components: {LinkIcon, MailIcon},
   data() {
     return {
       posts: [],
       authorName: {},
       author: {},
       isLoaded: false,
+      socialMediaLinks: {
+        facebook: null,
+        twitter: null,
+        instagram: null,
+        github: null,
+        linkedin: null,
+        displayEmail: null,
+        other: null,
+      },
     };
   },
   mounted() {
@@ -118,6 +161,10 @@ export default {
       await this.getBlogAuthor()
       this.author = this.author[0]
       this.isLoaded = true
+      console.log('This is the data in mounted')
+      console.log(this.author)
+      console.log('This are media links')
+      console.log(this.socialMediaLinks)
     });
   },
   methods: {
@@ -142,21 +189,35 @@ export default {
       this.author = await useAsyncData('getBlogAuthor', () => GqlGetBlogAuthor({author: this.$route.query.id}))
         .then(({data}) => {
           console.log(data._value.usersPermissionsUsers.data)
+          let socialMedia = data._value.usersPermissionsUsers.data[0].attributes.SocialMedia
+          console.log(socialMedia)
+          for(let i=0; i<socialMedia.length; i++){
+            if(socialMedia[i].__typename === 'ComponentSocialMediaFacebook')
+              this.socialMediaLinks.facebook = socialMedia[i].ProfileLink
+            else if(socialMedia[i].__typename === 'ComponentSocialMediaTwitter')
+              this.socialMediaLinks.twitter = socialMedia[i].ProfileLink
+            else if(socialMedia[i].__typename === 'ComponentSocialMediaInstagram')
+              this.socialMediaLinks.instagram = socialMedia[i].ProfileLink
+            else if(socialMedia[i].__typename === 'ComponentSocialMediaLinkedIn')
+              this.socialMediaLinks.linkedin = socialMedia[i].ProfileLink
+            else if(socialMedia[i].__typename === 'ComponentSocialMediaGitHub')
+              this.socialMediaLinks.github = socialMedia[i].ProfileLink
+            else if(socialMedia[i].__typename === 'ComponentSocialMediaDisplayEmail')
+              this.socialMediaLinks.displayEmail = socialMedia[i].DisplayEmail
+            else if(socialMedia[i].__typename === 'ComponentSocialMediaOther')
+              this.socialMediaLinks.other = socialMedia[i].ProfileLink
+          } 
           return data._value.usersPermissionsUsers.data.map((author) => ({
               name: author.attributes.name,
-              title: author.attributes.jobTitle,
-              company: author.attributes.company.replace('_', ' '),
+              fields: {
+                title: author.attributes.jobTitle,
+                company: author.attributes.partner.data.attributes.partner_name.replace('_', ' '),
+              },
               description: marked(author.attributes.description),
               photo:{
-                name: author.attributes.photo.data.attributes.name,
-                url: author.attributes.photo.data.attributes.url,
+                name:  author.attributes.photo.data ? author.attributes.photo.data.attributes.name : null ,
+                url:  author.attributes.photo.data ? author.attributes.photo.data.attributes.url : null ,
               }, 
-              displayEmail: author.attributes.displayEmail,
-              emailLink: 'mailto:' + author.attributes.displayEmail,
-              linkedin: author.attributes.linkedIn,
-              twitter: author.attributes.twitter,
-              facebook: author.attributes.facebook,
-              instagram: author.attributes.instagram,
           }))
         })
     },

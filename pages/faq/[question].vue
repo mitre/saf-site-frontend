838b5274b39ac4ae6759b033caea3c178074a39d
Christopher Hinchey <chinchey@mitre.org>
2022-10-27 13:47:55 -0400

    Cleaned up code
    Signed-off-by: Christopher Hinchey <chinchey@mitre.org>

--- pages/faq/[question].vue
+++ pages/faq/[question].vue
@@ -1,7 +1,6 @@
 <template>
   <Header /> 
   <div class="relative py-16 bg-white dark:bg-dark-bg overflow-hidden min-h-screen h-full">
-    
   <div class="absolute inset-0 bg-[url(/assets/grid.svg)] bg-top [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
     <div class="relative w-full bg-white dark:bg-gray-800 px-6 py-12 shadow-2xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:mx-auto md:max-w-3xl lg:max-w-4xl lg:pt-16 lg:pb-28">
     <div class="relative px-4 sm:px-6 lg:px-8">
@@ -32,45 +31,25 @@
 </template>
 
 <script>
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
-import { ChevronDownIcon, HashtagIcon } from '@heroicons/vue/outline'
-
 
 export default {
   data() {
     return {
       faq: {},
-      currentHash: "",
       isLoaded: false,
     };
   },
-  watch: {
-  $route() {
-      this.updateHash()
-  }
-  },
+
   mounted() {
     this.$nextTick(async () => {
       await this.getFAQ()
     });
   },
-  components: { ChevronDownIcon, HashtagIcon, Disclosure, DisclosureButton, DisclosurePanel },
   methods: {
-    slugify (str) {
-    str = str.toLowerCase()
-    str = str.trim()
-    str = str.replace(/[^\w\s-]/g, '')
-    str = str.replace(/[\s_-]+/g, '-')
-    str = str.replace(/^-+|-+$/g, '')
-    return str
-  },
     async getFAQ() {
       this.faq = await useAsyncData('getFaqByQuestionNumber', () => GqlGetFaqByQuestionNumber({number: parseInt(this.$route.params.question)}))
         .then(({data}) => {
-          console.log(data)
-          console.log(this.$route.params.question)
           var date = new Date(data._value.faqs.data[0].attributes.updatedAt)
-          this.currentHash = this.$route.hash.replace(/^#+/, '')
           this.isLoaded = true
           return {
             question: data._value.faqs.data[0].attributes.question,
@@ -81,9 +60,14 @@ export default {
           }
         });
     },
-    updateHash() {
-    this.currentHash = this.$route.hash.replace(/^#+/, '')
-    }
+    slugify (str) {
+      str = str.toLowerCase()
+      str = str.trim()
+      str = str.replace(/[^\w\s-]/g, '')
+      str = str.replace(/[.\s_-]+/g, '-')
+      str = str.replace(/^-+|-+$/g, '')
+      return str
+    },
   } 
 }
 </script>
--- pages/faq/index.vue
+++ pages/faq/index.vue
@@ -21,102 +21,30 @@
 <Footer />
 </template>
 
-<!-- <template>
-    <div>
-      <div>
-        <Header />
-      </div>
-      <div class="relative bg-white dark:bg-dark-bg min-h-screen h-full py-8 px-4 sm:px-6 lg:px-8">
-        <div class="relative max-w-7xl mx-auto">
-          <div class="mx-auto max-w-7xl px-4 sm:py-4 sm:px-6 lg:px-8">
-            <div class="text-center">
-              <p class="text-4xl font-bold tracking-tight dark:text-slate-200 sm:text-5xl lg:text-6xl">Frequently Asked Questions</p>
-              <p class="mx-auto mt-5 text-lg dark:text-gray-500">Have a question you don't see covered here? Please contact <a href="mailto:saf@groups.mitre.org" class="text-blue-500 dark:text-white">saf@groups.mitre.org</a></p>
-            </div>
-          </div>
-
-            <div class="mx-auto max-w-7xl pb-4 px-1 sm:pb-8 sm:px-6 lg:px-8">
-              <div class="mx-auto max-w-7xl divide-y-2 divide-gray-200">
-                <dl class="mt-6 space-y-6 divide-y divide-gray-200">
-                  <Disclosure  as="div" v-for="faq in faqs" :key="faq.question" class="pt-6 pb-3 sm:pb-0" v-slot="{ open }">
-                    <dt class="text-lg">
-                      <DisclosureButton class="flex w-full items-start justify-between text-left text-gray-400">
-                        <div class="flex h-2">
-                          <span >
-                            <a :href="`#${slugify(faq.question)}`"><HashtagIcon :class="slugify(faq.question) == currentHash ? 'dark:bg-gray-500 bg-gray-300 dark:text-white text-black rounded-full h-8 w-8 p-1' : 'dark:text-white text-black h-8 w-8 p-1'" aria-hidden="true"/></a>
-                          </span>
-                          <span class="ml-4 font-bold text-xl text-gray-800 dark:text-slate-200" :id="`${slugify(faq.question)}`">{{ faq.question }}</span>
-                        </div>
-                        <span class="ml-6 flex h-7 items-center">
-                          <ChevronDownIcon :class="[open || slugify(faq.question) == currentHash ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform']" aria-hidden="true" />
-                        </span>
-                      </DisclosureButton>
-                    </dt>
-                    <DisclosurePanel as="dd" class="mt-6 pr-12 pl-2">
-                      <span v-if="faq" v-html="faq.answer" class="mt-8 mx-auto leading-8 prose dark:prose-invert"></span>
-                    </DisclosurePanel>
-                  </Disclosure>
-                </dl>
-              </div>
-            </div>
-
-        </div>
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </div>
-  </template> -->
-
-  
-  <script>
-  import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
-  import { ChevronDownIcon, HashtagIcon } from '@heroicons/vue/outline'
-  
-  
-  export default {
-    data() {
-      return {
-        faqs: {},
-        currentHash: "",
-      };
+<script>
+export default {
+  data() {
+    return {
+      faqs: {},
+    };
+  },
+  mounted() {
+    this.$nextTick(async () => {
+      await this.getFAQs()
+    });
+  },
+  methods: {
+    async getFAQs() {
+      this.faqs = await useAsyncData('getAllFAQs', () => GqlFAQs())
+        .then(({ data }) => {
+          return data._value.faqs.data.map((faq) => ({
+            questionNumber: faq.attributes.question_number,
+            question: faq.attributes.question,
+            answer: faq.attributes.answer,
+          }))
+        });
     },
-    watch: {
-    $route() {
-        this.updateHash()
-    }
-    },
-    mounted() {
-      this.$nextTick(async () => {
-        await this.getFAQs()
-      });
-    },
-    components: { ChevronDownIcon, HashtagIcon, Disclosure, DisclosureButton, DisclosurePanel },
-    methods: {
-      slugify (str) {
-      str = str.toLowerCase()
-      str = str.trim()
-      str = str.replace(/[^\w\s-]/g, '')
-      str = str.replace(/[\s_-]+/g, '-')
-      str = str.replace(/^-+|-+$/g, '')
-      return str
-    },
-      async getFAQs() {
-        this.faqs = await useAsyncData('getAllFAQs', () => GqlFAQs())
-          .then(({ data }) => {
-            console.log(data)
-            this.currentHash = this.$route.hash.replace(/^#+/, '')
-            return data._value.faqs.data.map((faq) => ({
-              questionNumber: faq.attributes.question_number,
-              question: faq.attributes.question,
-              answer: faq.attributes.answer,
-            }))
-          });
-      },
-      updateHash() {
-     this.currentHash = this.$route.hash.replace(/^#+/, '')
-      }
-    } 
-  }
-  </script>
+  } 
+}
+</script>
   

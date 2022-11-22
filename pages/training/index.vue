1efbc86927c7a2bdf6dc53439c2d93e548a54d06
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2022-11-22 11:43:54 -0500

    Added handling for null case on data
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- pages/training/index.vue
+++ pages/training/index.vue
@@ -2,14 +2,15 @@
   <div>
     <Header />
       <div class="bg-white dark:bg-dark-bg min-h-screen h-full">
-        <div class ="relative w-full px-6 py-12 lg:pt-16 lg:pb-28 "> <!-- shadow-2xl shadow-slate-700/10 ring-1 ring-gray-900/5-->
-          <div class="md:flex">
+        <div class ="relative w-full px-6 py-12 lg:pt-16 lg:pb-28"> 
+          <div v-if="isLoaded" class="md:flex">
+            <!-- TODO: Put some kind of content or menu in left side bar -->
             <div id="Left Side Bar" class="mx-auto hidden min-w-fit md:block h-fit">
             </div>
-            <div id="Content Area" class="flex-1 items-center justify-center  ml-auto min-w-0 max-w-3xl">
+            <div id="Content Area" class="flex-1 items-center justify-center  ml-auto min-w-0 px-6 py-12 max-w-3xl sm:shadow-2xl shadow-slate-700/10 dark:shadow-slate-100/10 ring-3 ring-gray-900/5">
               <div id="Overview Section" class="sm:pl-4 md:max-w-3xl lg:max-w-3xl">
                 <h1 class="block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl"> Our Training</h1>
-                <p class="mt-6 dark:text-dark-text"> The MITRE SAF team regularly offers training courses. Dates and sign-up links for future courses are posted on this page when training dates are finalized. See below for course details, materials, and recordings.</p>
+                <p class="mt-6 dark:text-dark-text"> The MITRE SAF team offers training classes. Dates and sign-up links to participate in synchronous (typically virtual) class offerings are posted on this page when training dates are finalized. To preview classes, watch asynchronously, or reference class content, see the class details, materials, and recordings below.</p>
                 <div class="flex gap-8">
                   <a href="https://mitre.github.io/saf-training/" class="hover:underline">
                     <p class="mt-4 text-blue-500"> SAF Training Site - GitHub</p>
@@ -19,14 +20,14 @@
                   </a>
                 </div>
               </div>
-              <div id="Schedule for Meetings Mobile" class="relative pointer-events-auto order-2 pr-6 md:hidden">
+              <div id="Schedule for schedules Mobile" class="relative pointer-events-auto order-2 pt-8 pr-6 md:hidden">
                 <div  class="">
-                  <h2 class="text-lg font-semibold text-gray-900 sm:pl-3 pt-6 dark:text-white">Upcoming meetings</h2>
-                  <div v-if="meetings != null" class="lg:grid lg:grid-cols-2">
+                  <h2 class="text-lg font-semibold text-gray-900 sm:pl-3 pt-6 dark:text-white">Upcoming Classes</h2>
+                  <div v-if="schedules.length" class="lg:grid lg:grid-cols-2">
                     <ol class="mt-4 divide-y divide-gray-100 text-sm leading-6 lg:col-span-7 xl:col-span-8">
-                      <li v-for="meeting in meetings" :key="meeting.index" class="relative flex space-x-6 py-6 ">
+                      <li v-for="schedule in schedules" :key="schedule.index" class="relative flex space-x-6 py-6 ">
                         <div class="flex-0 pl-4">
-                          <h3 class="font-semibold text-gray-900 dark:text-MITRE-silver">{{ meeting.title }}</h3>
+                          <h3 class="font-semibold text-gray-900 dark:text-MITRE-silver">{{ schedule.title }}</h3>
                           <dl class="mt-2 flex flex-col text-gray-500 xl:flex-row">
                             <div class="flex items-start space-x-3 pl-1">
                               <dt class="mt-0.5">
@@ -34,27 +35,27 @@
                                 <CalendarIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
                               </dt>
                               <dd>
-                                <time :datetime="meeting.date"> {{formatDate(meeting.date)}} at {{formatTime(meeting.date)}} EST </time>
+                                <time :datetime="schedule.date"> {{formatDate(schedule.date)}} at {{formatTime(schedule.date)}} EST </time>
                               </dd>
                             </div>
                           </dl>
                         </div>
                         <div class="right-2 top-0 ">
-                          <a type="button" :href="meeting.link" target="_blank" class="inline-flex items-center rounded-full border border-transparent bg-blue-600 sm:px-3 sm:py-1.5 px-1 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Sign Up</a>
+                          <a type="button" :href="schedule.link" target="_blank" class="inline-flex items-center rounded-full border border-transparent bg-blue-600 sm:px-3 sm:py-1.5 px-1 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Sign Up</a>
                         </div>
                       </li>
                     </ol>
                   </div>
                   <div v-else class="relative max-w-sm mt-6 pl-2 dark:text-dark-text"> 
-                    <p> Sorry there are no upcoming meetings at this time. If you would like to host a meeting please contact saf@groups.mitre.org </p>
+                    <p> Sorry there are no upcoming schedules at this time. If you would like to host a schedule please contact saf@groups.mitre.org </p>
                   </div>
                 </div>
                 <hr class="mt-4 w-full" />
               </div>
-              <div id="Embeded Videos" v-if="courses != null" class="sm:flex-1 order-1 md:max-w-3xl pt-9">
+              <div id="Embeded Videos" class="sm:flex-1 order-1 md:max-w-3xl pt-9">
                 <h2 class="block text-3xl leading-8 font-bold tracking-tight text-gray-900 dark:text-white sm:text-3xl pt-4 sm:pl-4" > Courses </h2>
                 <hr class="mt-4 w-full" />
-                <div v-for="course in courses" :key="course.name">
+                <div v-if="courses.length" v-for="course in courses" :key="course.name">
                   <div class="sm:pl-4 mt-7">
                     <p class="text-xl font-semibold text-gray-900 dark:text-MITRE-silver">
                       {{ course.name }}
@@ -65,16 +66,19 @@
                     <hr class="mt-4 w-full" />
                   </div>
                 </div>
+                <div v-else>
+                  <p class="relative max-w-sm mt-6 pl-2 dark:text-dark-text"> Courses are being updated. Check back later! </p>
+                </div>
               </div>
             </div>
-            <div id="Schedule for Meetings" class="sticky top-28 pointer-events-auto pr-6 hidden min-w-fit pl-4 md:block h-fit">
+            <div id="Schedule for schedules" class="sticky top-28 pointer-events-auto pr-6 hidden max-w-xl min-w-min pl-4 md:block h-fit">
               <div  class="relative">
-                <h2 class="text-lg font-semibold text-gray-900 sm:pl-3 pt-6 dark:text-white">Upcoming meetings</h2>
-                <div v-if="meetings != null" class="lg:grid lg:grid-cols-2">
+                <h2 class="text-lg font-semibold text-gray-900 sm:pl-3 pt-6 dark:text-white">Upcoming Classes</h2>
+                <div v-if="schedules.length" class="lg:grid lg:grid-cols-2">
                   <ol class="mt-4 divide-y divide-gray-100 text-sm leading-6 lg:col-span-7 xl:col-span-8">
-                    <li v-for="meeting in meetings" :key="meeting.index" class="relative flex space-x-6 py-6 ">
+                    <li v-for="schedule in schedules" :key="schedule.index" class="relative flex space-x-6 py-6 ">
                       <div class="flex-0 pl-4">
-                        <h3 class="font-semibold text-gray-900 dark:text-MITRE-silver">{{ meeting.title }}</h3>
+                        <h3 class="font-semibold text-gray-900 dark:text-MITRE-silver">{{ schedule.title }}</h3>
                         <dl class="mt-2 flex flex-col text-gray-500 xl:flex-row">
                           <div class="flex items-start space-x-3 pl-1">
                             <dt class="mt-0.5">
@@ -82,52 +86,27 @@
                               <CalendarIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
                             </dt>
                             <dd>
-                              <time :datetime="meeting.date"> {{formatDate(meeting.date)}} at {{formatTime(meeting.date)}} EST </time> 
+                              <time :datetime="schedule.date"> {{formatDate(schedule.date)}} at {{formatTime(schedule.date)}} EST </time> 
                             </dd>
                           </div>
                         </dl>
                       </div>
                       <div class="right-2 top-0 ">
-                        <a type="button" :href="meeting.link" target="_blank" class="inline-flex items-center rounded-full border border-transparent bg-blue-600 sm:px-3 sm:py-1.5 px-1 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Sign Up</a>
+                        <a type="button" :href="schedule.link" target="_blank" class="inline-flex items-center rounded-full border border-transparent bg-blue-600 sm:px-3 sm:py-1.5 px-1 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Sign Up</a>
                       </div>
                     </li>
                   </ol>
                 </div>
-                <div v-else class="relative max-w-sm mt-6 pl-2 dark:text-dark-text"> 
-                  <p> Sorry there are no upcoming meetings at this time. If you would like to host a meeting please contact saf@groups.mitre.org </p>
+                <div v-else class="relative max-w-xs mt-6 pl-2 dark:text-dark-text"> 
+                  <p> Sorry there are no upcoming schedules at this time. If you would like to host a schedule please contact saf@groups.mitre.org </p>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-
-        <!-- TODO: Get Carousel component working -->
-        <!-- <div class="">
-          <div v-if="courses != null">
-            <Carousel v-slot="{currentSlide}" class="relative max-h-full h-full">
-              <Slide v-for="course in courses" :key="course.index">
-                <div v-show="currentSlide === course.index" class="  top-0 left-0 w-full max-h-full h-full" id="slide-info">
-                  <div id="Content" class="relative w-full sm:flex justify-center px-6 py-12 lg:pt-8 lg:pb-16">
-                    <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow-2xl max-w-2xl">
-                      <div class="px-4s py-5 sm:px-6">
-                        <p class="mt-3 text-center text-xl font-semibold text-gray-900 dark:text-dark-text">
-                        {{ course.name }} 
-                        </p>
-                      </div>
-                      <div class="px-4 py-5 sm:p-6">
-                        <div class="mt-8 leading-8 text-left prose prose-sm lg:prose-lg dark:prose-invert dark:text-dark-text prose-li:text-start prose-code:text-start"
-                          v-html="course.description">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </Slide>
-            </Carousel>
+          <div v-else>
+            <p> Loading... </p>
           </div>
-        </div> -->
-
-
+        </div>
       </div>
     <Footer />
   </div>
@@ -145,13 +124,17 @@ export default {
   data() {
     return {
       courses: [],
-      meetings: [],
+      schedules: [],
+      isLoaded: false,
     };
   },
   mounted() {
     this.$nextTick(async () => {
       await this.getTrainingData()
-      await this.getMeetingData()
+      await this.getScheduleData()
+      this.isLoaded = true
+      console.log('Here is empty data')
+      console.log(this.schedules)
     });
   },
   methods: {
@@ -166,15 +149,15 @@ export default {
           }))
         });
     },
-    async getMeetingData() {
-      this.meetings = await useAsyncData('getMeetingData', () => GqlGetMeetingData())
+    async getScheduleData() {
+      this.schedules = await useAsyncData('getScheduleData', () => GqlGetScheduleData())
         .then(({ data }) => {
-          console.log(data._value.courseMeetings.data)
-          return data._value.courseMeetings.data.map((meeting) => ({
-            title: meeting.attributes.title,
-            date: meeting.attributes.date,
-            link: meeting.attributes.link,
-            index: meeting.attributes.index,
+          console.log(data._value.courseSchedules.data)
+          return data._value.courseSchedules.data.map((schedule) => ({
+            title: schedule.attributes.title,
+            date: schedule.attributes.date,
+            link: schedule.attributes.link,
+            index: schedule.attributes.index,
           }))
         });
     },

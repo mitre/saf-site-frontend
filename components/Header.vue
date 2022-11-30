f34406099b50460bcab49416e2d8b753b9641514
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2022-11-30 12:44:29 -0500

    Fixed issues with dark mode
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- components/Header.vue
+++ components/Header.vue
@@ -7,10 +7,8 @@
           <div class="flex justify-left md:flex-1 lg:w-0 lg:flex-1">
             <a href="/">
               <span class="sr-only">Workflow</span>
-              <!-- <img class="h-8 w-auto sm:h-10" src="./public/saf_logo.svg" alt="" /> -->
               <svg class=" w-12 h-12 md:w-14 md:h-14 " viewBox="0 0 55 55" version="1.1"
                 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <!-- Generator: Sketch 55.2 (78181) - https://sketchapp.com -->
                 <title>SAF SVG - blue</title>
                 <desc>Created with Sketch.</desc>
                 <g id="SAF-SVG---blue" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -35,13 +33,13 @@
                 </g>
               </svg>
             </a>
-            <a href="/" class="flex">
+            <NuxtLink to="/" class="flex">
               <h1
                 class="text-2xl tracking-tighter p-2 font-extrabold whitespace-nowrap text-gray-900 dark:text-MITRE-silver  md:text-3xl xl:text-4xl">
                 <span class="block xl:inline">MITRE SAF</span> 
               </h1>
               <p class="mt-1 text-xs">©</p>
-            </a>
+            </NuxtLink>
           </div>
 
           <div id="mobile-components" class="-mr-2 -my-2 flex sm:pr-3 md:hidden">
@@ -192,6 +190,7 @@
           </PopoverGroup>
           <div class="hidden md:flex items-center justify-end flex-1 pr-2 md:w-0">
             <div class="hidden md:flex items-center justify-end flex-1 md:w-0">
+              <!-- Possible Sign in functionalilty -->
               <!-- <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 dark:hover:text-gray-200"> Sign in </a>
               <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-indigo-700"> Sign up </a> -->
               <div class="flex-1 flex justify-end px-2 lg:ml-6">
@@ -210,17 +209,27 @@
             </div>
             <div id="theme-toggle" type="button" 
               class="text-gray-500 dark:text-dark-text hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
-              @click="toggleTheme">
-              <svg id="theme-toggle-dark-icon" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hidden" fill="none"
-                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-              </svg>
-              <svg id="theme-toggle-light-icon" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hidden" fill="none"
-                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-              </svg>
+              >
+              <div class="relative flex items-center ">
+                <svg xmlns="http://www.w3.org/2000/svg" :class="selected == 'Dark' ?  'h-6 w-6' : 'hidden'" fill="none"
+                  viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" :class="selected =='Light' ?  'h-6 w-6' : 'hidden'" fill="none"
+                  viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" :class="selected == 'System' ?  'h-6 w-6' : 'hidden'" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                </svg>
+                <select name="theme" id="theme" class="absolute appearance-none pr-3 w-full h-fit opacity-0 dark:bg-slate-600" @change="switchSelect($event)">
+                  <option :selected="selected == 'Light'">Light</option>
+                  <option :selected="selected == 'Dark'">Dark</option>
+                  <option :selected="selected == 'System'">System</option>
+                </select >
+              </div>
             </div>
           </div>
         </div>
@@ -256,39 +265,8 @@
                   </nav>
                 </div>
               </div>
-              <!-- <div class="py-6 px-5 space-y-6">
-                <div class="grid grid-cols-2 gap-y-4 gap-x-8">
-                  <a href="#" class="text-base font-medium text-gray-900 dark:text-MITRE-silver hover:text-gray-700">
-                    Blog
-                  </a>
-                </nav>
-              </div>
-            </div>
-            <div class="py-6 px-5 space-y-6">
-              <div class="grid grid-cols-2 gap-y-4 gap-x-8">
-                <NuxtLink to="/blog" 
-                class="text-base font-medium text-gray-900 dark:text-MITRE-silver hover:text-gray-700">
-                  Blog
-                </NuxtLink>
-
-
-                  <a href="/docs" class="text-base font-medium text-gray-900 dark:text-MITRE-silver hover:text-gray-700">
-                    Docs
-                  </a>
-                  <a v-for="item in resources" :key="item.name" :href="item.href"
-                    class="text-base font-medium text-gray-900 dark:text-MITRE-silver hover:text-gray-700">
-                    {{ item.name }}
-                  </a>
-                  <a v-html="htmlCurrent" href="#" id="themeMobileMenuButton"
-                    class="text-base font-medium text-gray-900 dark:text-MITRE-silver hover:text-gray-700"
-                    @click='toggleTheme()'>
-
-
-                  </a>
-                </div>
-              </div> -->
               <div class="flex items-center justify-between py-6 px-4">
-                <label class="text-gray-900 text-base font-medium dark:text-MITRE-silver">Switch theme</label>
+                <label class="text-gray-900 text-base font-medium dark:text-MITRE-silver">Switch Theme</label>
                 <div class="relative flex items-center ring-1 ring-slate-900/10 rounded-lg shadow-sm p-2 text-slate-700 font-semibold dark:bg-slate-600 dark:ring-0 dark:highlight-white/5 dark:text-slate-200">
                   <svg viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6 mr-2 dark:hidden">
                     <path d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" class="stroke-slate-400 dark:stroke-slate-500"></path>
@@ -300,25 +278,22 @@
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z" class="fill-slate-400"></path>
                   </svg>{{selected}}<svg class="w-6 h-6 ml-2 text-slate-400" fill="none"><path d="m15 11-3 3-3-3" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                   </svg>
-                  <select id="theme" class="absolute appearance-none inset-0 w-full h-full opacity-0 dark:bg-slate-600" @change="switchSelect($event)">
+                  <!-- <svg id="theme-toggle-system-icon" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                   <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                  </svg> -->
+                  <select id="theme" class="absolute appearance-none inset-0 w-full h-full opacity-0 dark:bg-slate-600" @change="switchSelect($event.target.value)">
                     <option>Light</option>
                     <option>Dark</option>
                     <option>System</option>
                   </select>
                 </div>
-                  <!-- <a v-html="htmlCurrent" href="#" id="themeMobileMenuButton"
-                    class="text-base font-medium text-gray-900 dark:text-MITRE-silver hover:text-gray-700"
-                    @click='toggleTheme()'>
-
-                  </a> -->
-
               </div>
             </div>
           </PopoverPanel>
         </div>
       </transition>
     </Popover>
-    <!-- Check back later -->
+    <!-- TODO: Check back later on banner -->
     <!-- <div id="newsBannerDIV" class="relative bg-blue-600">
       <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div class="pr-16 text-center px-16">
@@ -462,51 +437,76 @@ export default {
       htmlDark: "Dark Mode",
       htmlLight: "Light Mode",
       htmlCurrent: "",
-      selected: "System",
+      selected: "",
+      options: ['Light', 'Dark', 'System'],
     };
   },
   methods: {
     switchSelect(event) {
-      console.log("Event: ", event.target.value);
+      console.log("switchSelect: ", event.target.value);
       if (event.target.value === 'Light') {
         this.disableDarkMode()
         localStorage.setItem('user-theme', 'light');
+        // localStorage.setItem('selectedValue', 'Light')
         this.selected = "Light"
+        this.setThemeState(this.selected)
       }
       else if (event.target.value == 'Dark'){
         this.enableDarkMode()
         localStorage.setItem('user-theme', 'dark');
+        // localStorage.setItem('selectedValue', 'Dark')
         this.selected = "Dark"
+        this.setThemeState(this.selected)
       }
       //TODO: Handle system option
+      else if (event.target.value == 'System')
+      {
+        const userIsInDarkModeOS = window.matchMedia('(prefers-color-scheme: dark)').matches
+        // localStorage.setItem('selectedValue', 'System')
+        if (userIsInDarkModeOS) {
+          this.enableDarkMode()
+          // console.log('System Dark!!!')
+          localStorage.setItem('user-theme', 'dark');
+          this.selected = "System";
+          this.setThemeState(this.selected)
+          // document.getElementById('theme-toggle-dark-icon').classList.add('hidden')
+          // document.getElementById('theme-toggle-light-icon').classList.add('hidden')
+          // document.getElementById('theme-toggle-system-icon').classList.remove('hidden')  
+        }
+        else {
+          this.disableDarkMode()
+          // console.log('System Light!!!')  
+          this.selected = "System";
+        }
+      }
+    },
+    setThemeState(mode){
+      // console.log(document.getElementById('theme'))
+      // document.getElementById('theme').setAttribute('theme', mode)
+      localStorage.setItem('theme', mode)
+    },
+    getThemeState(){
+      let theme = localStorage.getItem('theme')
+      console.log('THIS IS VASLUE')
+      console.log(theme)
+      this.selected = theme
+      // this.switchSelect(theme)
+      // const $select = document.querySelector('#theme')
+      // $select.value = theme
     },
     enableDarkMode() {
-      localStorage.setItem('color-theme', 'dark');
       document.documentElement.classList.add('dark');
-      document.getElementById('theme-toggle-dark-icon').classList.remove('hidden')
-      document.getElementById('theme-toggle-light-icon').classList.add('hidden')
-      // document.getElementById('themeMobileMenuButton').innerHTML = "Dark Mode"
+      // document.getElementById('theme-toggle-dark-icon').classList.remove('hidden')
+      // document.getElementById('theme-toggle-light-icon').classList.add('hidden')
+      // document.getElementById('theme-toggle-system-icon').classList.add('hidden')
       this.htmlCurrent = this.htmlDark
     },
     disableDarkMode() {
       document.documentElement.classList.remove('dark');
-      localStorage.setItem('color-theme', 'light');
-      document.getElementById('theme-toggle-light-icon').classList.remove('hidden')
-      document.getElementById('theme-toggle-dark-icon').classList.add('hidden')
-      //document.getElementById('themeMobileMenuButton').innerHTML = "Light Mode"
+      // document.getElementById('theme-toggle-light-icon').classList.remove('hidden')
+      // document.getElementById('theme-toggle-dark-icon').classList.add('hidden')
+      // document.getElementById('theme-toggle-system-icon').classList.add('hidden')
       this.htmlCurrent = this.htmlLight
-    },
-    toggleTheme() {
-      if (this.darkMode || localStorage.getItem("user-theme") === 'dark') {
-        this.disableDarkMode()
-        localStorage.setItem('user-theme', 'light');
-      } else {
-        this.enableDarkMode()
-        localStorage.setItem('user-theme', 'dark');
-      }
-
-      localStorage.setItem('userDidOverrideColor', 'true');
-      this.darkMode = !this.darkMode;
     },
     hideBanner() {
       var bannerDIV = document.getElementById("newsBannerDIV")
@@ -514,19 +514,45 @@ export default {
     },
   },
   mounted() {
-    const haveCustomColorScheme = localStorage.getItem("userDidOverrideColor") === 'true'
-    if (haveCustomColorScheme) {
-      console.log(localStorage.getItem("color-theme"))
-      localStorage.getItem("user-theme") === 'dark' ? this.enableDarkMode() : this.disableDarkMode()
-    } else {
+    
+    this.getThemeState()
+
+    if (this.selected === 'Light') {
+      this.disableDarkMode()
+      localStorage.setItem('user-theme', 'light');
+      // localStorage.setItem('selectedValue', 'Light')
+      this.selected = "Light"
+      this.setThemeState(this.selected)
+    }
+    else if (this.selected == 'Dark'){
+      this.enableDarkMode()
+      localStorage.setItem('user-theme', 'dark');
+      // localStorage.setItem('selectedValue', 'Dark')
+      this.selected = "Dark"
+      this.setThemeState(this.selected)
+    }
+    else if (this.selected == 'System')
+    {
       const userIsInDarkModeOS = window.matchMedia('(prefers-color-scheme: dark)').matches
+      // localStorage.setItem('selectedValue', 'System')
       if (userIsInDarkModeOS) {
-        this.darkMode = true;
         this.enableDarkMode()
+        localStorage.setItem('user-theme', 'dark');
+        this.selected = "System";
+        this.setThemeState(this.selected)
+        // document.getElementById('theme-toggle-dark-icon').classList.add('hidden')
+        // document.getElementById('theme-toggle-light-icon').classList.add('hidden')
+        // document.getElementById('theme-toggle-system-icon').classList.remove('hidden')  
       }
-      else
+      else {
         this.disableDarkMode()
+        this.selected = "System";
+        // document.getElementById('theme-toggle-dark-icon').classList.add('hidden')
+        // document.getElementById('theme-toggle-light-icon').classList.add('hidden')
+        // document.getElementById('theme-toggle-system-icon').classList.remove('hidden') 
+      }
     }
   }
+  
 };
 </script>

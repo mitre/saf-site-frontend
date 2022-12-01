7aa36ae0ab9cfda1ac3fba7708e8e214501b28d0
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2022-12-01 12:16:28 -0500

    Updated inline SVGs to increase code readability
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- components/Header.vue
+++ components/Header.vue
@@ -211,19 +211,9 @@
               class="text-gray-500 dark:text-dark-text hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
               >
               <div class="relative flex items-center ">
-                <svg xmlns="http://www.w3.org/2000/svg" :class="selected == 'Dark' ?  'h-6 w-6' : 'hidden'" fill="none"
-                  viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                  <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" :class="selected =='Light' ?  'h-6 w-6' : 'hidden'" fill="none"
-                  viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                  <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" :class="selected == 'System' ?  'h-6 w-6' : 'hidden'" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                </svg>
+                <SunIcon :class="selected =='Light' ?  'h-6 w-6' : 'hidden'" /> 
+                <MoonIcon :class="selected == 'Dark' ?  'h-6 w-6' : 'hidden'" /> 
+                <DesktopComputerIcon :class="selected == 'System' ?  'h-6 w-6' : 'hidden'" />
                 <select name="theme" id="theme" class="absolute appearance-none pr-3 w-full h-fit opacity-0 dark:bg-slate-600" @change="switchSelect($event)">
                   <option :selected="selected == 'Light'">Light</option>
                   <option :selected="selected == 'Dark'">Dark</option>
@@ -268,19 +258,9 @@
               <div class="flex items-center justify-between py-6 px-3">
                 <label class="text-gray-900 text-base font-medium dark:text-MITRE-silver">Switch Theme</label>
                 <div class="relative flex items-center ring-1 ring-slate-900/10 rounded-lg shadow-sm p-2 text-slate-700 font-semibold dark:bg-slate-600 dark:ring-0 dark:highlight-white/5 dark:text-slate-200">
-                  <svg xmlns="http://www.w3.org/2000/svg" :class="selected == 'Dark' ?  'h-6 w-6' : 'hidden'" fill="none"
-                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-                  </svg>
-                  <svg xmlns="http://www.w3.org/2000/svg" :class="selected =='Light' ?  'h-6 w-6' : 'hidden'" fill="none"
-                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-                  </svg>
-                  <svg xmlns="http://www.w3.org/2000/svg" :class="selected == 'System' ?  'h-6 w-6' : 'hidden'" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                  </svg>
+                  <SunIcon :class="selected =='Light' ?  'h-6 w-6' : 'hidden'" /> 
+                  <MoonIcon :class="selected == 'Dark' ?  'h-6 w-6' : 'hidden'" /> 
+                  <DesktopComputerIcon :class="selected == 'System' ?  'h-6 w-6' : 'hidden'" />
                   <div class="ml-2">{{selected}}</div>
                   <svg class="w-5 h-6  text-slate-400" fill="none"><path d="m15 11-3 3-3-3" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                   </svg>
@@ -351,7 +331,10 @@ import {
   ClipboardCheckIcon,
   CodeIcon,
   SearchIcon,
-  DotsVerticalIcon
+  DotsVerticalIcon,
+  DesktopComputerIcon,
+  MoonIcon,
+  SunIcon,
 } from '@heroicons/vue/outline';
 import { ChevronDownIcon } from '@heroicons/vue/solid';
 

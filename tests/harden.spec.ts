e5f09f28be610d714c34003c789ba39fb6f99fc2
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2023-02-06 12:58:01 -0500

    Removed comments and example files
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- tests/harden.spec.ts
+++ tests/harden.spec.ts
@@ -1,32 +1,3 @@
-
-
-// import { test, expect } from '@playwright/test';
-
-// test('test', async ({ page }) => {
-//   await page.goto('/');
-//   await page.getByRole('button', { name: 'Toolkit' }).click();
-//   await page.getByRole('link', { name: 'Harden Implement security baselines using our Ansible, Chef, and Terraform content.' }).click();
-//   await expect(page).toHaveURL('/toolkit/harden')
-//   page.waitForTimeout(5000)
-//   await page.getByRole('heading', { name: 'Harden' }).click();
-//   await page.getByText('Name').click();
-//   await page.getByText('Name').click();
-//   await page.getByPlaceholder('Search for ...').click();
-//   await page.getByPlaceholder('Search for ...').fill('aws');
-//   await page.getByRole('cell', { name: 'AWS CIS Benchmark | v1.1.0' }).click();
-//   await page.getByRole('button', { name: 'View Details , AWS CIS Benchmark' }).click();
-//   await page.getByText('CategoryCloud Service Providers').click();
-//   await page.getByText('AWS CIS Benchmark').click();
-//   await page.goto('/toolkit/harden');  
-//   await page.getByRole('row', { name: 'Docker CIS Benchmark | v1.1.0 ansible.svg Ansible saf_logo.svg MITRE SAF Github Logo View Code , Docker CIS Benchmark View Details , Docker CIS Benchmark' }).getByRole('cell', { name: 'Docker CIS Benchmark | v1.1.0' }).click();
-//   await page.getByRole('row', { name: 'Docker CIS Benchmark | v1.1.0 ansible.svg Ansible saf_logo.svg MITRE SAF Github Logo View Code , Docker CIS Benchmark View Details , Docker CIS Benchmark' }).getByRole('button', { name: 'View Details , Docker CIS Benchmark' }).click();
-//   await page.getByText('Docker CIS Benchmark').first().click();
-//   await page.goto('/toolkit/harden');
-//   await page.getByRole('cell', { name: 'Apache CIS Benchmark | v2.0.0' }).click();
-//   await page.getByRole('button', { name: 'View Details , Apache CIS Benchmark' }).click();
-//   // await page.getByText('Apache CIS Benchmark').click();
-// });
-
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {

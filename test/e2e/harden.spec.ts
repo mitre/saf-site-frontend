53ecc6f8a3381726fc6883bedfc33e8184b3cced
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2023-02-14 16:20:59 -0500

    Updated harden test
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- test/e2e/harden.spec.ts
+++ test/e2e/harden.spec.ts
@@ -23,5 +23,21 @@ test('Hardening', async ({ page }) => {
   await expect(page.getByText("AWS Foundations Benchmark")).toBeVisible();
   await expect(page.getByText("https://github.com/mitre/cis-aws-foundations-hardening")).toBeVisible();
   await expect(page.getByText("https://github.com/mitre/aws-foundations-cis-baseline")).toBeVisible();
+  await page.goto('/toolkit/harden');
+
+  await page.getByRole('button', { name: 'View Details , Tomcat 9 STIG' }).click();
+  await expect(page).toHaveURL('/toolkit/modal/tomcat-9-stig?id=77')
+  await expect(page.getByRole('heading', { level: 3, name: 'Guidance Overview' })).toBeVisible();
+  await expect(page.getByText("Tomcat 9 STIG")).toBeVisible();
+  await expect(page.getByText("https://github.com/ansible-lockdown/TOMCAT-9-STIG")).toBeVisible();
+  await page.goto('/toolkit/harden');
+
+  await page.getByRole('button', { name: 'View Details , Windows 2012 STIG' }).click();
+  await expect(page).toHaveURL('/toolkit/modal/windows-2012-stig?id=37')
+  await expect(page.getByRole('heading', { level: 3, name: 'Guidance Overview' })).toBeVisible();
+  await expect(page.getByText("Windows Server 2012/2012 R2 Member Server")).toBeVisible();
+  await expect(page.getByText("https://github.com/mitre/chef-stig-windows-2012-v2r12-memberserver-hardening")).toBeVisible();
+  await expect(page.getByText("https://github.com/mitre/microsoft-windows-2012r2-memberserver-stig-baseline")).toBeVisible();
+  await page.goto('/toolkit/harden');
   // TODO: Check more elements
 });

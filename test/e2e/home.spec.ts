cbcc18486dcde7a4a15a984d50ce08c3ad5e72cf
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-02-14 11:14:10 -0500

    Improved home test
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- test/e2e/home.spec.ts
+++ test/e2e/home.spec.ts
@@ -4,9 +4,9 @@ test('Home', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveURL('/')
 
-  
-  await page.getByText('MITRE SAF©').click();
-  await page.getByText('MITRE is a registered trademark of The MITRE Corporation. Material on this site may be copied and distributed with permission only.').click();
+  // Check header and footer text
+  await expect(page.getByText('MITRE SAF©')).toBeVisible();
+  await expect(page.getByText('MITRE is a registered trademark of The MITRE Corporation. Material on this site may be copied and distributed with permission only.')).toBeVisible();
   
   // Check theme selector
   await page.locator('#theme').selectOption('Dark');
@@ -14,21 +14,18 @@ test('Home', async ({ page }) => {
   await page.locator('#theme').selectOption('System');
   
 
-  // Test header navigation
+  // Test header visability
   await page.getByRole('button', { name: 'Toolkit' }).click();
-  await page.getByRole('link', { name: 'Plan' }).click();
-  await page.getByRole('button', { name: 'Toolkit' }).click();
-  await page.getByRole('link', { name: 'Harden' }).click();
-  await page.getByRole('button', { name: 'Toolkit' }).click();
-  await page.getByRole('link', { name: 'Validate' }).click();
-  await page.getByRole('button', { name: 'Toolkit' }).click();
-  await page.getByRole('link', { name: 'Normalize' }).click();
-  await page.getByRole('button', { name: 'Toolkit' }).click();
-  await page.getByRole('link', { name: 'Visualize' }).click();
-  await page.getByRole('link', { name: 'Blog' }).click();
-  await page.getByRole('link', { name: 'Docs' }).click();
+  await expect(page.getByRole('link', { name: 'Plan' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Harden' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Validate' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Normalize' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Visualize' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: 'Blog' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
+
   await page.getByRole('button', { name: 'More' }).click();
-  await page.getByRole('link', { name: 'FAQ' }).click();
-  await page.getByRole('button', { name: 'More' }).click();
-  await page.getByRole('link', { name: 'Training' }).click();
+  await expect(page.getByRole('link', { name: 'FAQ' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Training' })).toBeVisible();
 });

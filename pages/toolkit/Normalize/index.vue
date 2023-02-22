d7e0921ec2673d8694cbfef060b5433c6749abb6
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-02-22 17:25:39 -0500

    Add graphql calls for dynamic to/from hdf
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- pages/toolkit/Normalize/index.vue
+++ pages/toolkit/Normalize/index.vue
@@ -6,101 +6,68 @@
 
 
             <div v-if="isLoaded">
-                <div class="sm:flex sm:items-center">
-                    <div class="sm:flex-auto">
-                        <h1 class="text-4xl font-bold text-gray-900 dark:text-gray-50 text-center"><strong>{{
-                            pageTitle
-                        }}</strong>
-                        </h1>
-                        <div class="mt-8 mb-6 max-w-7xl mx-auto leading-8 text-left prose prose-sm lg:prose-lg dark:prose-invert dark:text-dark-text prose-li:text-start prose-code:text-start prose-pre:w-full prose-pre:h-[500px]"
-                            v-html="normalizeContent"></div>
+                <div class="">
 
-                        <div class="bg-white py-10 sm:py-12">
-                            <div class="mx-auto max-w-7xl px-6 lg:px-8">
-                                <div class="grid grid-cols-1 items-center gap-x-8 gap-y-16 lg:grid-cols-2">
-                                    <div class="mx-auto w-full max-w-xl lg:mx-0">
-                                        <h2 class="text-3xl font-bold tracking-tight text-gray-900">Convert To These Common
-                                            Types
-                                        </h2>
-                                        <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum dolor sit amet,
-                                            consectetur
-                                            adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet
-                                            ullamcorper eu enim et fermentum, augue.</p>
-                                        <div class="mt-8 flex items-center gap-x-6">
-                                            <!-- <a href="#"
+                    <h1 class="text-4xl font-bold text-gray-900 dark:text-gray-50 text-center"><strong>{{
+                        pageTitle
+                    }}</strong>
+                    </h1>
+
+                    <!-- First Part of Text Content -->
+                    <div class="mt-8 mb-6 max-w-7xl mx-auto leading-8 text-left prose prose-sm lg:prose-lg dark:prose-invert dark:text-dark-text prose-li:text-start prose-code:text-start prose-pre:w-full prose-pre:h-[500px]"
+                        v-html="normalizeContent"></div>
+
+                    <!-- HDF Convert Logo Clouds -->
+                    <div class="py-10">
+                        <div class="mx-auto max-w-7xl">
+                            <div class="mb-5">
+                                <h2 class="text-3xl font-bold tracking-tight text-gray-900">Get Security Data From
+                                    Multiple
+                                    Sources
+                                </h2>
+                                <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum dolor sit amet,
+                                    consectetur
+                                    adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet
+                                    ullamcorper eu enim et fermentum, augue.</p>
+                                <div class="mt-8 flex items-center gap-x-6">
+                                    <!-- <a href="#"
                                                                         class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create
                                                                         account</a> -->
-                                            <a href="https://saf-cli.mitre.org/#usage"
-                                                class="text-sm font-semibold text-gray-900">View More<span
-                                                    aria-hidden="true">&rarr;</span></a>
-                                        </div>
-                                    </div>
-
-                                    <div
-                                        class="mx-auto grid w-full max-w-xl grid-cols-5 items-center gap-y-1 sm:gap-y-2 lg:mx-0 lg:max-w-none lg:pl-4">
-                                        <div v-for="element in fromHDF">
-                                            <div
-                                                class="w-[100px] h-[100px] object-contain object-center backdrop-opacity-10 backdrop-invert bg-white/30 p-4 rounded">
-                                                <span class="text-black opacity-100">{{ element }}</span>
-                                            </div>
-                                        </div>
-                                        <!-- <img class="max-h-12 w-full object-contain object-left"
-                                                                    src="https://tailwindui.com/img/logos/tuple-logo-gray-900.svg" alt="Tuple" width="105"
-                                                                    height="48" /> -->
-
-                                    </div>
-
+                                    <a href="https://saf-cli.mitre.org/#usage"
+                                        class="text-sm font-semibold text-gray-900">View More<span
+                                            aria-hidden="true">&rarr;</span></a>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="bg-white py-10 sm:py-12">
-                            <div class="mx-auto max-w-7xl px-6 lg:px-8">
-                                <div class="grid grid-cols-1 items-center gap-x-8 gap-y-16 lg:grid-cols-2">
-
-
-                                    <div
-                                        class="mx-auto grid w-full max-w-xl grid-cols-5 items-center gap-y-1 sm:gap-y-2 lg:mx-0 lg:max-w-none lg:pl-4">
+                            <div class="flex flex-col items-center w-full">
+                                <div
+                                    class="w-[100px] h-[100px] object-contain object-center backdrop-opacity-10 backdrop-invert bg-white/30 p-2 rounded">
+                                    <span class="text-black opacity-100">HDF</span>
+                                </div>
+                                <ArrowDownIcon class="h-10 w-10 text-black my-4" />
+                                <div class="grid grid-cols-1 items-center">
+                                    <div class="mx-auto grid w-full max-w-xl grid-cols-5 gap-4">
                                         <div v-for="element in toHDF">
                                             <div
-                                                class="w-[100px] h-[100px] object-contain object-center backdrop-opacity-10 backdrop-invert bg-white/30 p-2 rounded">
-                                                <span class="text-black opacity-100">{{ element }}</span>
+                                                class="w-[100px] h-[100px] backdrop-opacity-10 backdrop-invert bg-white/30 p-2 rounded">
+                                                <span class="text-black opacity-100">{{ element.attributes.name }}</span>
                                             </div>
                                         </div>
                                         <!-- <img class="max-h-12 w-full object-contain object-left"
                                                                     src="https://tailwindui.com/img/logos/tuple-logo-gray-900.svg" alt="Tuple" width="105"
                                                                     height="48" /> -->
-
                                     </div>
-
-                                    <div class="mx-auto w-full max-w-xl lg:mx-0">
-                                        <h2 class="text-3xl font-bold tracking-tight text-gray-900">Get Security Data From
-                                            Multiple
-                                            Sources
-                                        </h2>
-                                        <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum dolor sit amet,
-                                            consectetur
-                                            adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet
-                                            ullamcorper eu enim et fermentum, augue.</p>
-                                        <div class="mt-8 flex items-center gap-x-6">
-                                            <!-- <a href="#"
-                                                                        class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create
-                                                                        account</a> -->
-                                            <a href="https://saf-cli.mitre.org/#usage"
-                                                class="text-sm font-semibold text-gray-900">View More<span
-                                                    aria-hidden="true">&rarr;</span></a>
-                                        </div>
-                                    </div>
-
                                 </div>
+
                             </div>
+
                         </div>
-
-
-                        <div class="mt-8 mb-6 max-w-7xl mx-auto leading-8 text-left prose prose-sm lg:prose-lg dark:prose-invert dark:text-dark-text prose-li:text-start prose-code:text-start prose-pre:w-full prose-pre:h-[500px]"
-                            v-html="normalizeContent2"></div>
-
                     </div>
+
+                    <!-- Second Part of Text Content -->
+                    <div class="mt-8 mb-6 max-w-7xl mx-auto leading-8 text-left prose prose-sm lg:prose-lg dark:prose-invert dark:text-dark-text prose-li:text-start prose-code:text-start prose-pre:w-full prose-pre:h-[500px]"
+                        v-html="normalizeContent2"></div>
+
+
                 </div>
             </div>
             <div v-else>
@@ -112,13 +79,16 @@
 </template>
 
 <script setup>
+import { ArrowDownIcon } from '@heroicons/vue/solid';
 ////  Data  ////
 const isLoaded = ref(false)
 const normalizeContent = ref("")
 const normalizeContent2 = ref("")
 const pageTitle = ref("")
-const fromHDF = ref(["ASFF", "Splunk", "XCCDF Results", "Checklist", "CSV", "Condensed JSON"])
-const toHDF = ref(["ASFF", "AWS", "Burp Suite", "CKL to POA&M", "DBProtect", "Fortify", "GoSec", "Ion Channel", "JFrog Xray", "Tenable Nessus", "Netsparker", "Nikto", "Prisma", "Prowler", "Sarif", "Scoutsuite", "Snyk", "SonarQube", "Splunk", "Trivy", "Twistlock", "XCCDF", "OWASP ZAP"])
+const fromHDF = ref([])
+const toHDF = ref([])
+// const fromHDF = ref(["ASFF", "Splunk", "XCCDF Results", "Checklist", "CSV", "Condensed JSON"])
+// const toHDF = ref(["ASFF", "AWS", "Burp Suite", "CKL to POA&M", "DBProtect", "Fortify", "GoSec", "Ion Channel", "JFrog Xray", "Tenable Nessus", "Netsparker", "Nikto", "Prisma", "Prowler", "Sarif", "Scoutsuite", "Snyk", "SonarQube", "Splunk", "Trivy", "Twistlock", "XCCDF", "OWASP ZAP"])
 
 ////  Methods  ////
 const getNormalizeContent = async () => {
@@ -127,7 +97,18 @@ const getNormalizeContent = async () => {
             pageTitle.value = data._value.textContents.data[0].attributes.page
             normalizeContent.value = data._value.textContents.data.find(item => item.attributes.name === "HDF Summary Info").attributes.text
             normalizeContent2.value = data._value.textContents.data.find(item => item.attributes.name === "Inspec HDF Examples").attributes.text
+        });
 
+    await useAsyncData('getConvertToHDFs', () => GqlGetConvertToHDFs(), { initialCache: false })
+        .then(({ data }) => {
+            toHDF.value = data._value.convertToHdfs.data
+            console.log(toHDF.value)
+        });
+
+    await useAsyncData('getConvertFromHDFs', () => GqlGetConvertFromHDFs(), { initialCache: false })
+        .then(({ data }) => {
+            fromHDF.value = data._value.convertFromHdfs.data
+            console.log(fromHDF.value)
         });
 }
 

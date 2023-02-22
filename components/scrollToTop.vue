a3052a0b5e275a3c8c052966d932587096c5a09c
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2023-02-22 16:10:21 -0500

    Scroll behavior test
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- components/scrollToTop.vue
+++ components/scrollToTop.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-3xl">
-    <div ref="scrollTopButton"  class="hidden w-fit justify-end fixed right-0 bottom-0 pb-3 pr-2 transition">
+    <div ref="scrollTopButton"  class=" w-fit justify-end fixed right-0 bottom-0 pb-3 pr-2 transition">
       <div class="text-blue-500 hover:text-blue-400 transition">
         <button @click="scrollToTop" class="flex items-center" >
           <ArrowCircleUpIcon class="w-8 h-10 md:w-10 md:h-10 text-blue-500" />
@@ -15,24 +15,24 @@
 export default {
   components: { ArrowCircleUpIcon },
 
-  mounted() {
-    window.addEventListener("scroll", this.handleScroll);
-  },
+  // mounted() {
+  //   window.addEventListener("scroll", this.handleScroll);
+  // },
 
-  beforeUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-  },
+  // beforeUnmount() {
+  //   window.removeEventListener("scroll", this.handleScroll);
+  // },
 
   methods: {
-    handleScroll() {
-      const scrollBtn = this.$refs.scrollTopButton;
+    // handleScroll() {
+    //   const scrollBtn = this.$refs.scrollTopButton;
 
-      if (window.scrollY > 0) {
-        scrollBtn.classList.remove("hidden");
-      } else {
-        scrollBtn.classList.add("hidden");
-      }
-    },
+    //   if (window.scrollY > 0) {
+    //     scrollBtn.classList.remove("hidden");
+    //   } else {
+    //     scrollBtn.classList.add("hidden");
+    //   }
+    // },
     scrollToTop() {
     window.scrollTo({ top: 0, behavior: "smooth" });
     },

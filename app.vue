b3d876913b19996220332b985773e45cc4125c16
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2023-02-22 15:33:28 -0500

    Removed unneeded comments
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- app.vue
+++ app.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="bg-gray-50 dark:bg-dark-bg overscroll-none" >
     <NuxtLayout>
-      <!-- <Header /> -->
       <NuxtLoadingIndicator />
       <!-- <NuxtLoadingBar :duration="1000" /> -->
       <NuxtPage />
       <scrollToTop />
-      <!-- <Footer /> -->
     </NuxtLayout>
   </div>
 </template> 

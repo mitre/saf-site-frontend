5cfbbe5f31c99cc50bbc59167b97a4ac82799ed0
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-02-23 14:31:51 -0500

    Minor styling fixes
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- pages/toolkit/Normalize/index.vue
+++ pages/toolkit/Normalize/index.vue
@@ -17,7 +17,7 @@
                     <div class="mt-8 mb-6 max-w-7xl mx-auto leading-8 text-left prose prose-sm lg:prose-lg dark:prose-invert dark:text-dark-text prose-li:text-start prose-code:text-start prose-pre:w-full prose-pre:h-[500px]"
                         v-html="normalizeContent"></div>
 
-                    <!-- HDF Convert Logo Clouds -->
+                    <!-- HDF Convert To Logo Clouds -->
                     <div class="py-10">
                         <div class="mx-auto max-w-7xl">
                             <div class="mb-5">
@@ -36,22 +36,67 @@
                                     <a href="https://saf-cli.mitre.org/#usage"
                                         class="text-sm font-semibold dark:text-gray-200">View More<span
                                             aria-hidden="true">&rarr;</span></a>
-                                    <a href="/toolkit/normalize/schemas"
-                                        class="text-sm font-semibold dark:text-gray-200">View
-                                        Schemas<span aria-hidden="true">&rarr;</span></a>
+                                </div>
+                            </div>
+                            <div class="flex flex-col items-center w-full">
+
+                                <div class="grid grid-cols-1 items-center">
+                                    <div class="mx-auto grid w-full max-w-xl grid-cols-5 gap-4">
+                                        <div v-for="element in toHDF">
+                                            <div
+                                                class="w-[60px] h-[60px] md:w-[100px] md:h-[100px] backdrop-opacity-10 backdrop-invert bg-white/30 p-2 rounded text-center flex justify-center items-center">
+                                                <span class="text-black opacity-100 object-fill">{{ element.attributes.name
+                                                }}</span>
+                                            </div>
+                                        </div>
+                                        <!-- <img class="max-h-12 w-full object-contain object-left"
+                                                src="https://tailwindui.com/img/logos/tuple-logo-gray-900.svg" alt="Tuple" width="105"
+                                                height="48" /> -->
+                                    </div>
+                                </div>
+                                <ArrowDownIcon class="h-10 w-10 text-black dark:text-gray-200 my-4" />
+                                <div
+                                    class="w-[60px] h-[60px] md:w-[100px] md:h-[100px] object-contain object-center backdrop-opacity-10 backdrop-invert bg-white/30 p-2 rounded text-center flex justify-center items-center">
+                                    <span class="text-black opacity-100">HDF</span>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <!-- HDF Convert To Logo Clouds -->
+                    <div class="py-10">
+                        <div class="mx-auto max-w-7xl">
+                            <div class="mb-5">
+                                <h2 class="text-3xl font-bold tracking-tight dark:text-gray-100">Convert To These Common
+                                    Tools
+                                </h2>
+                                <p class="mt-6 text-lg leading-8 dark:text-dark-text">Lorem ipsum dolor sit amet,
+                                    consectetur
+                                    adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet
+                                    ullamcorper eu enim et fermentum, augue.</p>
+                                <div class="mt-8 flex items-center gap-x-6">
+                                    <!-- <a href="#"
+                                                                        class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create
+                                                                        account</a> -->
+                                    <a href="https://saf-cli.mitre.org/#usage"
+                                        class="text-sm font-semibold dark:text-gray-200">View More<span
+                                            aria-hidden="true">&rarr;</span></a>
                                 </div>
                             </div>
                             <div class="flex flex-col items-center w-full">
                                 <div
-                                    class="w-[60px] h-[60px] md:w-[100px] md:h-[100px] object-contain object-center backdrop-opacity-10 backdrop-invert bg-white/30 p-2 rounded text-center">
+                                    class="w-[60px] h-[60px] md:w-[100px] md:h-[100px] object-contain object-center backdrop-opacity-10 backdrop-invert bg-white/30 p-2 rounded text-center flex justify-center items-center">
                                     <span class="text-black opacity-100">HDF</span>
                                 </div>
                                 <ArrowDownIcon class="h-10 w-10 text-black dark:text-gray-200 my-4" />
                                 <div class="grid grid-cols-1 items-center">
                                     <div class="mx-auto grid w-full max-w-xl grid-cols-5 gap-4">
-                                        <div v-for="element in toHDF">
+                                        <div v-for="element in fromHDF">
                                             <div
-                                                class="w-[60px] h-[60px] md:w-[100px] md:h-[100px] backdrop-opacity-10 backdrop-invert bg-white/30 p-2 rounded text-center">
+                                                class="w-[60px] h-[60px] md:w-[100px] md:h-[100px] backdrop-opacity-10 backdrop-invert bg-white/30 p-2 rounded text-center flex justify-center items-center">
                                                 <span class="text-black opacity-100 object-fill">{{ element.attributes.name
                                                 }}</span>
                                             </div>

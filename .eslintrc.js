c7ea2bd538239eecebc3d750073f760e37513847
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-03-14 15:08:51 -0400

    Added globals for .eslintrc.js
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,11 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false,
+    requireConfigFile: false
   },
   extends: [
     'plugin:vue/essential',
@@ -21,20 +21,44 @@ module.exports = {
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:vue/vue3-recommended',
-    "plugin:nuxt/recommended",
-    'prettier', // Make sure this is the last
+    'plugin:nuxt/recommended',
+    'prettier' // Make sure this is the last
   ],
-  plugins: [
-    'vue',
-  ],
+  plugins: ['vue'],
+  globals: {
+    // Add GraphQL functions as globals to fix no-undef eslint errors
+    GqlGetBlogDataFromAuthor: 'readonly',
+    GqlGetBlogDataFromID: 'readonly',
+    GqlGetBlogAuthor: 'readonly',
+    GqlBlogPosts: 'readonly',
+    GqlGetIndexDocumentation: 'readonly',
+    GqlGetDocumentation: 'readonly',
+    GqlGetFaqByQuestionNumber: 'readonly',
+    GqlGetTrainingData: 'readonly',
+    GqlGetScheduleData: 'readonly',
+    GqlGetGuidanceDataFromID: 'readonly',
+    GqlGetValidateData: 'readonly',
+    GqlGetHardenData: 'readonly',
+    GqlFAQs: 'readonly',
+    useNuxtApp: 'readonly',
+    useRoute: 'readonly',
+    useAsyncData: 'readonly',
+    navigateTo: 'readonly',
+    nextTick: 'readonly',
+    onBeforeUnmount: 'readonly',
+    onMounted: 'readonly',
+    reactive: 'readonly',
+    ref: 'readonly',
+    toRefs: 'readonly'
+  },
   overrides: [
     {
-      "files": ["*.{ts,tsx}"],
-      "parser": "@typescript-eslint/parser",
-      "plugins": ["@typescript-eslint"],
-      "extends": ["plugin:@typescript-eslint/recommended"]
+      files: ['*.{ts,tsx}'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: ['plugin:@typescript-eslint/recommended']
     }
   ],
   // add your custom rules here
-  rules: {},
+  rules: {}
 };

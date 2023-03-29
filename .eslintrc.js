a89a612b3728b254416bbdd56a23e8627efb9a45
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2023-03-29 15:16:00 -0400

    Linting Upgrade (#107)
    * Added airbnb eslint rules
    
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>
    
    * Added ts support and nuxt rules for linter
    
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>
    
    * Changed package.json and .eslintrc.js
    
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>
    
    * Corrected package.json format script
    
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>
    
    * Fixed package.json lint script
    
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>
    
    * Added globals for .eslintrc.js
    
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>
    
    * Updated yarn.lock
    
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>
    
    * Updated package and yarn lock
    
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>
    
    * Updated dependencies
    
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>
    Co-authored-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,64 @@
 module.exports = {
+  root: true,
   env: {
+    browser: true,
     node: true
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
-  rules: {
-    // Override or add rules settings here
-    // Example: 'vue/no-unused-vars': 'error'
-    // "no-console": ["error", { allow: ["warn", "error"] }]
-  }
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
+  },
+  extends: [
+    'plugin:vue/essential',
+    './node_modules/eslint-config-airbnb-base/rules/best-practices.js',
+    './node_modules/eslint-config-airbnb-base/rules/errors.js',
+    './node_modules/eslint-config-airbnb-base/rules/es6.js',
+    './node_modules/eslint-config-airbnb-base/rules/imports.js',
+    './node_modules/eslint-config-airbnb-base/rules/node.js',
+    './node_modules/eslint-config-airbnb-base/rules/strict.js',
+    './node_modules/eslint-config-airbnb-base/rules/style.js',
+    './node_modules/eslint-config-airbnb-base/rules/variables.js',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:vue/vue3-recommended',
+    'plugin:nuxt/recommended',
+    'prettier' // Make sure this is the last
+  ],
+  plugins: ['vue'],
+  globals: {
+    // Add GraphQL functions as globals to fix no-undef eslint errors
+    GqlGetBlogDataFromAuthor: 'readonly',
+    GqlGetBlogDataFromID: 'readonly',
+    GqlGetBlogAuthor: 'readonly',
+    GqlBlogPosts: 'readonly',
+    GqlGetIndexDocumentation: 'readonly',
+    GqlGetDocumentation: 'readonly',
+    GqlGetFaqByQuestionNumber: 'readonly',
+    GqlGetTrainingData: 'readonly',
+    GqlGetScheduleData: 'readonly',
+    GqlGetGuidanceDataFromID: 'readonly',
+    GqlGetValidateData: 'readonly',
+    GqlGetHardenData: 'readonly',
+    GqlFAQs: 'readonly',
+    useNuxtApp: 'readonly',
+    useRoute: 'readonly',
+    useAsyncData: 'readonly',
+    navigateTo: 'readonly',
+    nextTick: 'readonly',
+    onBeforeUnmount: 'readonly',
+    onMounted: 'readonly',
+    reactive: 'readonly',
+    ref: 'readonly',
+    toRefs: 'readonly'
+  },
+  overrides: [
+    {
+      files: ['*.{ts,tsx}'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: ['plugin:@typescript-eslint/recommended']
+    }
+  ],
+  // add your custom rules here
+  rules: {}
 };

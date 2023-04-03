b6fdb75b8e93a49c2436094943178836aa9129ea
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-04-03 16:24:01 -0400

    Minor height and styling change
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- components/FrameworkPage.vue
+++ components/FrameworkPage.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="relative bg-white dark:bg-dark-bg h-full pt-4 px-4 sm:px-6 lg:px-8">
-        <div class="mx-auto max-w-7xl text-center mb-11">
+    <div class="relative bg-white dark:bg-dark-bg pt-4 px-4 sm:px-6 lg:px-8 min-h-[calc(100vh-295px)] mb-11">
+        <div class="mx-auto max-w-7xl text-center">
             <img :src="`${productIcon}`" class="w-14 md:w-20 aspect-square mx-auto my-2" aria-hidden="true"
                 alt="Alt text here" />
             <h2 class="text-3xl font-bold tracking-tight dark:text-gray-200 sm:text-4xl">{{
                 title }}</h2>
             <h3 class="mt-6 mx-auto leading-8 text-center dark:text-dark-text">{{ grabber }}</h3>
-            <div class="mt-8 mx-auto leading-8 text-left dark:text-dark-text min-h-screen" v-html="description">
+            <div class="mt-8 mx-auto leading-8 text-left dark:text-dark-text" v-html="description">
             </div>
 
         </div>

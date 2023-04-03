46e15303f5a30fc952601466d4272e09f1dc8e4f
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-04-03 15:47:52 -0400

    Add min-height to framework page desc.
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- components/FrameworkPage.vue
+++ components/FrameworkPage.vue
@@ -6,7 +6,8 @@
             <h2 class="text-3xl font-bold tracking-tight dark:text-gray-200 sm:text-4xl">{{
                 title }}</h2>
             <h3 class="mt-6 mx-auto leading-8 text-center dark:text-dark-text">{{ grabber }}</h3>
-            <div class="mt-8 mx-auto leading-8 text-left dark:text-dark-text" v-html="description"></div>
+            <div class="mt-8 mx-auto leading-8 text-left dark:text-dark-text min-h-[30vh] mb-2" v-html="description">
+            </div>
 
         </div>
     </div>

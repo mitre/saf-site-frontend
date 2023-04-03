97fb33cb4d545bdfc550183ed4e32efe54c185a3
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-04-03 15:21:30 -0400

    Changed some nav to proper locations
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- test/e2e/harden.spec.ts
+++ test/e2e/harden.spec.ts
@@ -18,7 +18,7 @@ test('Hardening', async ({ page }) => {
 
   // Check guidance
   await page.getByRole("button", {name: "view Details"}).click();
-  await expect(page).toHaveURL('/framework/guidance/aws-cis-benchmark?id=83')
+  await expect(page).toHaveURL('/libs/guidance/aws-cis-benchmark?id=83')
   await expect(page.getByRole('heading', { level: 3, name: 'Guidance Overview' })).toBeVisible();
   await expect(page.getByText("AWS Foundations Benchmark")).toBeVisible();
   await expect(page.getByText("https://github.com/mitre/cis-aws-foundations-hardening")).toBeVisible();
@@ -26,14 +26,14 @@ test('Hardening', async ({ page }) => {
   await page.goto('/framework/harden');
 
   await page.getByRole('button', { name: 'View Details , Tomcat 9 STIG' }).click();
-  await expect(page).toHaveURL('/framework/guidance/tomcat-9-stig?id=77')
+  await expect(page).toHaveURL('/libs/guidance/tomcat-9-stig?id=77')
   await expect(page.getByRole('heading', { level: 3, name: 'Guidance Overview' })).toBeVisible();
   await expect(page.getByText("Tomcat 9 STIG")).toBeVisible();
   await expect(page.getByText("https://github.com/ansible-lockdown/TOMCAT-9-STIG")).toBeVisible();
   await page.goto('/framework/harden');
 
   await page.getByRole('button', { name: 'View Details , Windows 2012 STIG' }).click();
-  await expect(page).toHaveURL('/framework/guidance/windows-2012-stig?id=37')
+  await expect(page).toHaveURL('/libs/guidance/windows-2012-stig?id=37')
   await expect(page.getByRole('heading', { level: 3, name: 'Guidance Overview' })).toBeVisible();
   await expect(page.getByText("Windows Server 2012/2012 R2 Member Server")).toBeVisible();
   await expect(page.getByText("https://github.com/mitre/chef-stig-windows-2012-v2r12-memberserver-hardening")).toBeVisible();

827049e15d70c323a6d2c5300d2a9972ac244038
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-04-11 17:37:52 -0400

    Started to add some possible tailwind color variables
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   darkMode: 'class',
   content: [
-  './index.html', 
-  './src/**/*.{vue,js,ts,jsx,tsx}',    
-  "./components/**/*.{js,vue,ts}",
-  "./layouts/**/*.vue",
-  "./pages/**/*.vue",
-  "./plugins/**/*.{js,ts}",
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
   ],
   theme: {
-    extend: { 
+    extend: {
       colors: {
         "MITRE-blue": "var(--color-MITRE-blue)",
         "MITRE-highlighter": "var(--color-MITRE-highlighter)",
@@ -27,6 +27,47 @@ module.exports = {
         "dark-bg": "var(--color-DARKMODE-dark-bg)",
         "dark-text": "var(--color-DARKMODE-dark-text)",
       },
+      // colors: {
+      //   'dark-highlighter': '',
+      //   'light-highlighter': '',
+      //   'dark-text': '',
+      //   'light-text': '',
+      //   'dark-focus': '',
+      //   'light-focus': '',
+      //   'dark-code': '',
+      //   'light-code': '',
+      //   'dark-backdrop': '',
+      //   'light-backdrop': '',
+      //   mitre: {
+      //     blue: '#005B94',
+      //     highlighter: '#FFF601',
+      //     'dark-navy': '#0B2338',
+      //     navy: '#0D2F4F',
+      //     'light-blue': '#87DEFF',
+      //     black: '#111921',
+      //     'dark-gray': '#7E8284',
+      //     silver: '#D4D4D3',
+      //     'light-silver': '#F1F3F4',
+      //   },
+      //   btn: {
+      //     'light-primary': '',
+      //     'dark-primary': '',
+      //     'light-secondary': '',
+      //     'dark-secondary': '',
+      //     'light-tertiary': '',
+      //     'dark-tertiary': '',
+      //     'light-danger': '',
+      //     'dark-danger': '',
+      //   },
+      //   nav: {
+      //     'light-active': '',
+      //     'dark-active': '',
+      //     'light-inactive': '',
+      //     'dark-inactive': '',
+      //     'light-disabled': '',
+      //     'dark-disabled': '',
+      //   }
+      // },
       fontFamily: {
         sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
       },

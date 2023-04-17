84274e176aee0d7266356affad712067ee4bdeed
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-04-17 15:43:09 -0400

    Fixed harden and validate test
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- test/e2e/harden.spec.ts
+++ test/e2e/harden.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test';
 test('Hardening', async ({ page }) => {
   // Navigate to harden page
   await page.goto('/');
-  await page.getByRole('button', { name: 'Framework' }).click();
+  await page.getByRole('button', { name: 'Libraries' }).click();
   await page.getByRole('link', { name: 'Harden' }).click();
-  await expect(page).toHaveURL('/framework/harden')
+  await expect(page).toHaveURL('/libs/harden')
 
   await expect(page.getByRole('heading', { level: 1, name: 'Harden' })).toBeVisible();
   // Filter table
@@ -23,14 +23,14 @@ test('Hardening', async ({ page }) => {
   await expect(page.getByText("AWS Foundations Benchmark")).toBeVisible();
   await expect(page.getByText("https://github.com/mitre/cis-aws-foundations-hardening")).toBeVisible();
   await expect(page.getByText("https://github.com/mitre/aws-foundations-cis-baseline")).toBeVisible();
-  await page.goto('/framework/harden');
+  await page.goto('/libs/harden');
 
   await page.getByRole('button', { name: 'View Details , Tomcat 9 STIG' }).click();
   await expect(page).toHaveURL('/libs/guidance/tomcat-9-stig?id=77')
   await expect(page.getByRole('heading', { level: 3, name: 'Guidance Overview' })).toBeVisible();
   await expect(page.getByText("Tomcat 9 STIG")).toBeVisible();
   await expect(page.getByText("https://github.com/ansible-lockdown/TOMCAT-9-STIG")).toBeVisible();
-  await page.goto('/framework/harden');
+  await page.goto('/libs/harden');
 
   await page.getByRole('button', { name: 'View Details , Windows 2012 STIG' }).click();
   await expect(page).toHaveURL('/libs/guidance/windows-2012-stig?id=37')
@@ -38,5 +38,5 @@ test('Hardening', async ({ page }) => {
   await expect(page.getByText("Windows Server 2012/2012 R2 Member Server")).toBeVisible();
   await expect(page.getByText("https://github.com/mitre/chef-stig-windows-2012-v2r12-memberserver-hardening")).toBeVisible();
   await expect(page.getByText("https://github.com/mitre/microsoft-windows-2012r2-memberserver-stig-baseline")).toBeVisible();
-  await page.goto('/framework/harden');
+  await page.goto('/libs/harden');
 });

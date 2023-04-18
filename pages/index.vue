09010c6e30fdaa6be7c75ccf4a13f4c039b54649
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2023-04-18 13:22:35 -0400

    Updated sponsor subtitles
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- pages/index.vue
+++ pages/index.vue
@@ -14,12 +14,12 @@
         <div class="lg:max-w-7xl mx-auto py-24 sm:py-32 lg:py-40" >
           <p class="mt-2 mb-6 md:mb-2  text-3xl text-center font-bold tracking-tight text-blue-700 sm:text-4xl ">Adopted by The Community</p>
           <div class="md:flex items-center pt-8 ml-4">
-            <p class="font-bold tracking-tight text-gray-900 dark:text-gray-50 text-2xl sm:text-3xl mx-4 items-center">Sponsors</p>
-            <HomeSponsors :data="sponsors" class="mx-auto" />
+            <p class="font-bold text-center sm:mx-4 tracking-tight text-gray-900 dark:text-gray-50 text-2xl sm:text-3xl  items-center">Sponsors</p>
+            <HomeSponsors :data="sponsors" class="mx-auto mt-4" />
           </div>
           <div class="md:flex items-center ml-4 mt-4">
-            <p class="md:hidden font-bold tracking-tight text-gray-900 dark:text-gray-50 text-2xl sm:text-3xl ml-8  items-center">Vendors</p>
-            <HomeSponsors :data="vendors" class="mx-auto" />
+            <p class="md:hidden text-center font-bold tracking-tight text-gray-900 dark:text-gray-50 text-2xl sm:text-3xl ml-8  items-center">Vendors</p>
+            <HomeSponsors :data="vendors" class="mx-auto mt-4" />
             <p class="hidden md:flex font-bold tracking-tight text-gray-900 dark:text-gray-50 sm:text-3xl items-center mx-4 mr-8">Vendors</p>
           </div>
         </div>

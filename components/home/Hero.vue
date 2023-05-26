ab6ff1f39af51e0f343cb07bf941a655fc7c9f82
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-05-26 16:15:04 -0400

    Added new pipeline image
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-light-backdrop-primary dark:bg-dark-backdrop-primary pt-24 px-6">
+  <div class="bg-light-backdrop-primary dark:bg-dark-backdrop-primary pt-12 px-6">
     <main>
       <h1
         class="text-3xl tracking-tight font-extrabold text-light-header dark:text-dark-header sm:text-5xl md:text-6xl text-center">
@@ -16,7 +16,7 @@
               sense for your environment.</p>
           </div>
           <div class="mt-16 sm:mt-24">
-            <img src="@/assets/saf-pipeline-light.svg"
+            <img src="@/assets/saf-PIPE-diagram-embedded-fonts.svg"
               alt="Diagram of multistage pipeline and the SAF components that support each stage" width="2432"
               height="1442" />
           </div>

cbb8cccc8feabe1561952825b9a1cce7fc825dc0
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-05-30 17:29:21 -0400

    Fixed application v-html issue
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- components/ApplicationPage.vue
+++ components/ApplicationPage.vue
@@ -6,7 +6,9 @@
             <h2 class="text-3xl font-bold tracking-tight text-light-header dark:text-dark-header sm:text-4xl">{{
                 title }}</h2>
             <h3 class="mt-6 mx-auto leading-8 text-center text-light-text dark:text-dark-text">{{ grabber }}</h3>
-            <div class="mt-8 mx-auto leading-8 text-left text-light-text dark:text-dark-text" v-html="description"></div>
+            <div class="prose max-w-none mt-8 mx-auto leading-8 text-left text-light-text dark:text-dark-text"
+                v-html="description">
+            </div>
 
         </div>
     </div>

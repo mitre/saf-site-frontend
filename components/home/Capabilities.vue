f8c0a875c5a13cf712b314f8ee69ec526bad0764
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-05-31 17:34:16 -0400

    Minor styling fix
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- components/home/Capabilities.vue
+++ components/home/Capabilities.vue
@@ -13,7 +13,7 @@
         <div
           class="divide-y divide-light-border-primary dark:divide-dark-border-primary overflow-hidden rounded-lg shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0 mt-8">
           <div v-for="(capability) in capabilities.slice(0, -1)" :key="capability.orderID"
-            :class="[capability.orderID === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '', capability.orderID === 1 ? 'sm:rounded-tr-lg' : '', capability.orderID === capabilities.length - 2 ? 'sm:rounded-bl-lg' : '', capability.orderID === capabilities.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '', 'group relative bg-white dark:bg-gray-700 p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue-500 hover:bg-gray-100 dark:hover:bg-gray-800']">
+            :class="[capability.orderID === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '', capability.orderID === 1 ? '' : '', capability.orderID === capabilities.length - 2 ? 'sm:rounded-bl-lg' : '', capability.orderID === capabilities.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '', 'group relative bg-white dark:bg-gray-700 p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue-500 hover:bg-gray-100 dark:hover:bg-gray-800']">
             <div>
               <div class="inline-flex rounded-lg p-3 ring-4 ring-light-border-primary dark:ring-dark-border-primary">
                 <img :src="capability.icon.url" :alt="capability.icon.name"

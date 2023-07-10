68473b6e19ecc7ba281b694573374ae9a059f5fa
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-07-10 01:35:16 -0400

    Changed Harden and Validate top content text size

--- pages/libs/Harden.vue
+++ pages/libs/Harden.vue
@@ -10,7 +10,7 @@
         <div class="sm:flex sm:items-center">
           <div class="sm:flex-auto">
             <h1 class="text-4xl font-bold text-header">Harden</h1>
-            <p class="mt-2 text-base">These open-source community-based hardening
+            <p class="mt-2">These open-source community-based hardening
               baselines help to securely configure common system components.
               MITRE is helping to provide stewardship over these hardening baselines, hosted here and at other community
               vendor sites.

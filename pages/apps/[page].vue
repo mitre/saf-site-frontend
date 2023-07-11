2e62b603da47b720b3176894591feac0afb33c75
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-07-11 15:51:40 -0400

    Added typescript to apps page
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- pages/apps/[page].vue
+++ pages/apps/[page].vue
@@ -1,18 +1,14 @@
 <template>
   <div>
+
     <Head>
       <Title>{{ pageTitle }}</Title>
       <Meta name="description" :content="`Application page for ${pageTitle}`" />
     </Head>
     <Header />
     <div v-if="isLoaded">
-      <ApplicationPage
-        :product-icon="pageIconHref"
-        :title="pageTitle"
-        :grabber="pageGrabber"
-        :description="pageContent"
-        :features="pageFeatures"
-      />
+      <ApplicationPage :product-icon="pageIconHref" :title="pageTitle" :grabber="pageGrabber" :description="pageContent"
+        :features="pageFeatures" />
     </div>
     <div v-else class="grid h-screen place-items-center">
       <LoadingSpinner />
@@ -21,7 +17,8 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+
 /// /  Data  ////
 const isLoaded = ref(false);
 const route = useRoute();
@@ -29,27 +26,30 @@ const pageIconHref = ref('');
 const pageTitle = ref('');
 const pageContent = ref('');
 const pageGrabber = ref('');
-const pageFeatures = ref([]);
+const pageFeatures = ref();
 
 /// /  Methods  ////
 const getPageContent = async () => {
   await useAsyncData('getApplicationPage', () =>
-    GqlGetApplicationPage({page: route.params.page.replaceAll('-', ' ')})
-  ).then(({data}) => {
-    pageIconHref.value =
-      data.value.appPages.data[0].attributes.tool.data.attributes.icon.data.attributes.url;
-    pageTitle.value =
-      data.value.appPages.data[0].attributes.tool.data.attributes.name;
-    pageGrabber.value = data.value.appPages.data[0].attributes.grabber;
-    pageContent.value = data.value.appPages.data[0].attributes.description;
-    pageFeatures.value = data.value.appPages.data[0].attributes.features.map(
-      (item) => ({
-        name: item.title,
-        description: item.description,
-        imageSrc: item.image.data.attributes.url,
-        imageAlt: item.image.data.attributes.alternativeText
-      })
-    );
+    GqlGetApplicationPage({ page: route.fullPath.split('/')[2].replaceAll('-', ' ') })
+  ).then(({ data }) => {
+    const pageData = data?.value?.appPages?.data[0].attributes
+    if (pageData) {
+      pageIconHref.value =
+        pageData?.tool?.data?.attributes?.icon?.data?.attributes?.url ?? "Error";
+      pageTitle.value =
+        data?.value?.appPages?.data[0]?.attributes?.tool?.data?.attributes?.name ?? "Error";
+      pageGrabber.value = data?.value?.appPages?.data[0]?.attributes?.grabber ?? "Error";
+      pageContent.value = data?.value?.appPages?.data[0]?.attributes?.description ?? "Error";
+      pageFeatures.value = data?.value?.appPages?.data[0]?.attributes?.features?.map(
+        (item) => ({
+          name: item?.title,
+          description: item?.description,
+          imageSrc: item?.image?.data?.attributes?.url,
+          imageAlt: item?.image?.data?.attributes?.alternativeText
+        })
+      ) ?? [{ name: "Error", description: "Error", imageSrc: "Error", imageAlt: "Error" }];
+    }
     return data;
   });
 };

df1e340cfd2bf37b0563cfae44ecc44ad8bc592d
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-07-10 20:40:48 -0400

    Updated some nav descriptions
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- components/Header.vue
+++ components/Header.vue
@@ -52,7 +52,7 @@
             <div class="hidden xl:flex items-center justify-end flex-1 md:w-0">
               <!-- TODO: Site search-->
             </div>
-            <div class="text-base  hover:bg-neutral-2  rounded-lg text-sm p-2.5">
+            <div class="hover:bg-neutral-2  rounded-lgp-2.5">
               <div class="relative flex items-center ">
                 <SunIcon :class="selected == 'Light' ? 'h-6 w-6' : 'hidden'" />
                 <MoonIcon :class="selected == 'Dark' ? 'h-6 w-6' : 'hidden'" />
@@ -156,10 +156,8 @@ import {
 } from '@headlessui/vue';
 import { ref, markRaw } from 'vue';
 import {
-  ShieldCheckIcon,
   QuestionMarkCircleIcon,
   XIcon,
-  ClipboardCheckIcon,
   DotsVerticalIcon,
   UserGroupIcon,
   DesktopComputerIcon,
@@ -186,7 +184,7 @@ const framework = markRaw([
   {
     name: 'Plan',
     description:
-      'Choose, tailor, and create security guidance appropriate for your mission.',
+      'Choose, tailor, and create security configuration guidance appropriate for your mission.',
     href: '/framework/plan',
     icon: PlanIcon
 
@@ -194,7 +192,7 @@ const framework = markRaw([
   {
     name: 'Harden',
     description:
-      'Implement security baselines using our Ansible, Chef, and Terraform content.',
+      'Take action to configure software for security.',
     href: '/framework/harden',
     icon: HardenIcon
   },
@@ -215,7 +213,7 @@ const framework = markRaw([
   {
     name: 'Visualize',
     description:
-      'Identify overall security status and deep-dive to solve specific security defects',
+      'View comprehensive security status, identify security defects, and manage remediation.',
     href: '/framework/visualize',
     icon: VisualizeIcon
   }
@@ -237,49 +235,52 @@ const libraries = ref([
     icon: ValidationLibIcon
   },
   {
+    name: 'HDF Converters',
+    description:
+      '?????',
+    href: '/apps/hdf-converters',
+    icon: SafShieldLogo
+  },
+  {
     name: 'TS InSpec Objects',
     description:
       '??????????',
     href: '/libs/ts-inspec-objects',
-    icon: ClipboardCheckIcon
+    icon: SafShieldLogo
   },
   {
     name: 'eMASS Client',
     description:
       '?????????',
     href: '/libs/emass-client',
-    icon: ClipboardCheckIcon
+    icon: SafShieldLogo
   }
 ]);
 
-const applications = ref([
+const applications = shallowRef([
   {
     name: 'Heimdall',
-    description:
-      '?????',
+    description: "Security Data Visualization App",
     href: '/apps/heimdall',
     icon: HeimdallLogo
   },
   {
     name: 'Vulcan',
-    description:
-      '?????',
+    description: "Security Guidance Authorship App",
     href: '/apps/vulcan',
     icon: SafShieldLogo
   },
   {
     name: 'SAF CLI',
-    description:
-      '?????',
+    description: "Support utility for security automation",
     href: '/apps/saf-cli',
-    icon: ShieldCheckIcon
+    icon: SafShieldLogo
   },
   {
     name: 'eMASSer',
-    description:
-      '?????',
+    description: "Automate interactions with eMASS",
     href: '/apps/emasser',
-    icon: ShieldCheckIcon
+    icon: SafShieldLogo
   },
 ]);
 

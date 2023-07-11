fb8fe5c6f2a90f3def40fb519714480acfcbe62d
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-07-11 16:20:37 -0400

    Changed eslint config and remove extra packages
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,81 +4,23 @@ module.exports = {
     browser: true,
     node: true
   },
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    parser: '@typescript-eslint/parser'
   },
   extends: [
     'plugin:vue/essential',
-    './node_modules/eslint-config-airbnb-base/rules/best-practices.js',
-    './node_modules/eslint-config-airbnb-base/rules/errors.js',
-    './node_modules/eslint-config-airbnb-base/rules/es6.js',
-    './node_modules/eslint-config-airbnb-base/rules/imports.js',
-    './node_modules/eslint-config-airbnb-base/rules/node.js',
-    './node_modules/eslint-config-airbnb-base/rules/strict.js',
-    './node_modules/eslint-config-airbnb-base/rules/style.js',
-    './node_modules/eslint-config-airbnb-base/rules/variables.js',
-    'plugin:import/errors',
-    'plugin:import/warnings',
     'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
     'plugin:nuxt/recommended',
     'prettier' // Make sure this is the last
   ],
-  plugins: ['vue'],
-  globals: {
-    // Add GraphQL functions as globals to fix no-undef eslint errors
-    GqlGetBlogDataFromAuthor: 'readonly',
-    GqlGetBlogDataFromID: 'readonly',
-    GqlGetBlogAuthor: 'readonly',
-    GqlBlogPosts: 'readonly',
-    GqlGetAllDocumentation: 'readonly',
-    GqlGetIndexDocumentation: 'readonly',
-    GqlGetDocumentation: 'readonly',
-    GqlGetFaqByQuestionNumber: 'readonly',
-    GqlGetTrainingData: 'readonly',
-    GqlGetScheduleData: 'readonly',
-    GqlGetGuidanceDataFromID: 'readonly',
-    GqlGetValidateData: 'readonly',
-    GqlGetHardenData: 'readonly',
-    GqlGetGettingStartedPage: 'readonly',
-    GqlGetToolset: 'readonly',
-    GqlGetCapabilities: 'readonly',
-    GqlGetSponsors: 'readonly',
-    GqlGetVendors: 'readonly',
-    GqlGetUserStories: 'readonly',
-    GqlGetTenets: 'readonly',
-    GqlFAQs: 'readonly',
-    GqlGetApplicationPage: 'readonly',
-    GqlGetFrameworkPage: 'readonly',
-    GqlGetLibraryPageByName: 'readonly',
-    useNuxtApp: 'readonly',
-    useRoute: 'readonly',
-    useAsyncData: 'readonly',
-    navigateTo: 'readonly',
-    nextTick: 'readonly',
-    onBeforeUnmount: 'readonly',
-    onMounted: 'readonly',
-    reactive: 'readonly',
-    ref: 'readonly',
-    toRefs: 'readonly'
-  },
-  overrides: [
-    {
-      files: ['*.{ts,tsx}'],
-      parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint'],
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:import/typescript'
-      ]
-    }
-  ],
-  // add your custom rules here
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
+    'prettier/prettier': 'error',
     'vue/multi-word-component-names': 'off',
-    'vue/no-v-model-argument': 'off', // This should be supported in Vue 3 and seems to break command palette code
-    'no-restricted-syntax': 'off'
+    // not needed for vue 3
+    'vue/no-multiple-template-root': 'off'
   }
 };

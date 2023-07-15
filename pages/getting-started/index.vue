e3c222a0bc8a4977850a7c0a933be631effe440e
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-07-15 05:43:38 -0400

    Added typing to getting started page
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- pages/getting-started/index.vue
+++ pages/getting-started/index.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
+
     <Head>
       <Title>Getting Started</Title>
       <Meta name="description" content="Getting started content for SAF" />
     </Head>
     <div>
       <Header />
-      <div
-        class="max-w-8xl relative h-full min-h-screen bg-neutral-1 px-4 pt-4 sm:px-6 lg:px-8"
-      >
-        <div v-if="isLoaded">
+      <div class="max-w-8xl relative h-full min-h-screen bg-neutral-1 px-4 pt-4 sm:px-6 lg:px-8">
+        <div v-if="isLoaded && gettingStartedContent">
           <div class="sm:flex sm:items-center">
             <div class="sm:flex-auto">
               <h1 class="text-center text-4xl font-bold text-header">
@@ -17,14 +16,12 @@
               </h1>
               <div
                 class="prose-img prose prose-sm mx-auto mb-6 mt-8 max-w-5xl text-left leading-8 dark:prose-invert lg:prose-lg prose-code:text-start prose-li:text-start"
-                style=""
-                v-html="gettingStartedContent"
-              ></div>
+                style="" v-html="gettingStartedContent"></div>
             </div>
           </div>
         </div>
         <div v-else>
-          <p>Loading ...</p>
+          <LoadingSpinner />
         </div>
         <Footer />
       </div>
@@ -32,17 +29,17 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 /// /  Data  ////
 const isLoaded = ref(false);
-const gettingStartedContent = ref('');
+const gettingStartedContent = ref<string | undefined>();
 
 /// /  Methods  ////
 const getGettingStartedContent = async () => {
   gettingStartedContent.value = await useAsyncData(
     'getGettingStartedPage',
     () => GqlGetGettingStartedPage()
-  ).then(({data}) => data.value.gettingStartedPage.data.attributes.content);
+  ).then(({ data }) => data?.value?.gettingStartedPage?.data?.attributes?.content);
 };
 
 /// /  Lifecycle  ////

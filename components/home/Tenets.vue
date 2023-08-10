47ca8093e9a5f3411c0c445a1a573334d07e4a4f
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-08-10 17:23:06 -0400

    Added open source logo to tenets
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- components/home/Tenets.vue
+++ components/home/Tenets.vue
@@ -3,43 +3,24 @@
     <div class="relative py-24 sm:py-32 lg:py-40">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl lg:text-center">
-          <h1
-            class="mt-2 text-3xl font-bold tracking-tight text-header sm:text-4xl"
-          >
+          <h1 class="mt-2 text-3xl font-bold tracking-tight text-header sm:text-4xl">
             The SAF Is
           </h1>
         </div>
         <div class="mt-24 grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-3">
-          <div
-            v-for="tenet in tenets"
-            :key="tenet.orderID"
-            class="flex max-w-sm gap-y-4 lg:mx-auto"
-          >
+          <div v-for="tenet in tenets" :key="tenet.orderID" class="flex max-w-sm gap-y-4 lg:mx-auto">
             <div class="px-2">
               <div class="flex items-center">
-                <CurrencyDollarIcon
-                  v-if="tenet.name === 'Free'"
-                  class="mr-2 h-12 w-12"
-                />
-                <BeakerIcon
-                  v-else-if="tenet.name === 'Open Source'"
-                  class="mr-2 h-12 w-12"
-                />
-                <GlobeIcon
-                  v-else-if="tenet.name === 'Community Project'"
-                  class="mr-2 h-12 w-12"
-                />
+                <CurrencyDollarIcon v-if="tenet.name === 'Free'" class="mr-2 h-12 w-12" />
+                <OsiKeyhole v-else-if="tenet.name === 'Open Source'"
+                  class="mr-2 h-12 w-12 dark:fill-white fill-black dark:stroke-white stroke-black" />
+                <GlobeIcon v-else-if="tenet.name === 'Community Project'" class="mr-2 h-12 w-12" />
                 <AnnotationIcon v-else class="mr-2 h-12 w-12" />
-                <div
-                  class="text-start text-2xl font-extrabold tracking-tight sm:text-3xl"
-                >
+                <div class="text-start text-2xl font-extrabold tracking-tight sm:text-3xl">
                   {{ tenet.name }}
                 </div>
               </div>
-              <div
-                class="prose dark:prose-invert mt-3 text-start text-lg leading-7"
-                v-html="tenet.description"
-              />
+              <div class="prose dark:prose-invert mt-3 text-start text-lg leading-7" v-html="tenet.description" />
             </div>
           </div>
         </div>
@@ -55,7 +36,8 @@ import {
   BeakerIcon,
   AnnotationIcon
 } from '@heroicons/vue/outline';
-import {Tenet} from 'global';
+import OsiKeyhole from '@/assets/icons/OsiKeyhole.vue';
+import { Tenet } from 'global';
 
 const props = defineProps({
   tenets: {
@@ -64,5 +46,5 @@ const props = defineProps({
   }
 });
 
-const {tenets} = toRefs(props);
+const { tenets } = toRefs(props);
 </script>

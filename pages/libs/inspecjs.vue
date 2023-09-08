286661ad62399352a1a4023fcb92bc21e5139a5c
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-09-08 17:09:46 -0400

    Added inspec js & stig library pages
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- pages/libs/inspecjs.vue
+++ pages/libs/inspecjs.vue
@@ -16,7 +16,7 @@ const pageTitle = ref('');
 const getLibraryContent = async () => {
   await useAsyncData(
     'getLibraryPageByName',
-    () => GqlGetLibraryPageByName({name: 'eMASS Client'}),
+    () => GqlGetLibraryPageByName({name: 'InSpecJS'}),
     {initialCache: false}
   ).then(({data}) => {
     libraryContent.value = data.value.libraryPages.data[0].attributes.content;
--- pages/libs/ohdf-converters.vue
+++ pages/libs/ohdf-converters.vue
@@ -1,34 +1,9 @@
 <template>
-  <div>
-    <Head>
-      <Title>OHDF Converters</Title>
-      <Meta name="description" content="HDF Converters library content" />
-    </Head>
-    <div>
-      <Header />
-      <div
-        class="max-w-8xl relative h-full min-h-screen bg-neutral-1 px-4 pt-4 sm:px-6 lg:px-8"
-      >
-        <div v-if="isLoaded">
-          <div class="sm:flex sm:items-center">
-            <div class="sm:flex-auto">
-              <h1 class="text-center text-5xl font-bold text-header">
-                <strong>{{ pageTitle }}</strong>
-              </h1>
-              <div
-                class="prose prose-xl prose-headings:text-foreground prose-a:text-foreground mx-auto mb-6 mt-8 max-w-5xl text-left leading-8 text-foreground"
-                v-html="libraryContent"
-              ></div>
-            </div>
-          </div>
-        </div>
-        <div v-else>
-          <p>Loading ...</p>
-        </div>
-        <Footer />
-      </div>
-    </div>
-  </div>
+  <LibraryPage
+    :is-loaded="isLoaded"
+    :page-title="pageTitle"
+    :library-content="libraryContent"
+  />
 </template>
 
 <script setup>

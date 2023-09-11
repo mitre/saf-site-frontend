08d396b70dd5b5d2a3632693c712f437941e9876
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-09-11 16:01:35 -0400

    Updates

---
+++ _nuxt/redirect.24917a2f.js
@@ -0,0 +1,1 @@
+import{cn as o}from"./entry.12648e91.js";const a=o((e,t)=>{console.log("What are these: ",e,t)});export{a as default};
--- assets/redirect.e3e1c6fd.js
+++
@@ -1,1 +0,0 @@
-import{cn as o}from"./entry.aa641b54.js";const a=o((e,t)=>{console.log("What are these: ",e,t)});export{a as default};

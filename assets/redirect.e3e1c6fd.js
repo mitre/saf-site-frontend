fb6221c3ae6eb26e0eef96f0f2d14c86f6189c30
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-09-11 15:48:31 -0400

    Updates

---
+++ assets/redirect.e3e1c6fd.js
@@ -0,0 +1,1 @@
+import{cn as o}from"./entry.aa641b54.js";const a=o((e,t)=>{console.log("What are these: ",e,t)});export{a as default};
---
+++ assets/useSlugify.31da7e80.js
@@ -0,0 +1,1 @@
+function t(r){let e=r;return e=e.toLowerCase(),e=e.trim(),e=e.replace(/[^\w\s-]/g,""),e=e.replace(/[\s_-]+/g,"-"),e=e.replace(/^-+|-+$/g,""),e}export{t as s};

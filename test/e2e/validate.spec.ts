f8a1f03aa756923c3ac6c76d32840a7462687f23
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-10-05 23:35:48 -0400

    Fixed failing tests
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- test/e2e/validate.spec.ts
+++ test/e2e/validate.spec.ts
@@ -24,55 +24,58 @@ test('Validation Library Page', async ({page}) => {
   await page.getByRole('button', {name: 'view Details'}).click();
   await expect(page).toHaveURL('/libs/guidance/gke-cis-benchmark?id=6');
   await expect(
-    page.getByRole('heading', {level: 3, name: 'Guidance Overview'})
+    page.getByRole('heading', {level: 1, name: 'Guidance Overview'})
   ).toBeVisible();
-  await expect(page.getByText('GKE CIS Benchmark')).toBeVisible();
   await expect(
-    page.getByText(
-      'https://github.com/GoogleCloudPlatform/inspec-gke-cis-benchmark'
+    page.getByText('Google Kubernetes Engine CIS Benchmark')
+  ).toBeVisible();
+  await expect(
+    page.locator(
+      '[href*="https://github.com/GoogleCloudPlatform/inspec-gke-cis-benchmark"]'
     )
   ).toBeVisible();
+
   await page.goto('/libs/validate');
+
+  await page.getByPlaceholder('Search for ...').click();
+  await page.getByPlaceholder('Search for ...').fill('ubuntu');
 
   await page
     .getByRole('button', {name: 'View Details , Ubuntu 16.04 STIG'})
     .click();
   await expect(page).toHaveURL('/libs/guidance/ubuntu-1604-stig?id=27');
   await expect(
-    page.getByRole('heading', {level: 3, name: 'Guidance Overview'})
+    page.getByRole('heading', {level: 1, name: 'Guidance Overview'})
   ).toBeVisible();
-  await expect(page.getByText('Ubuntu 16.04 STIG').first()).toBeVisible();
+  await expect(page.getByText('Ubuntu 16.04 Linux STIG').first()).toBeVisible();
   await expect(
-    page.getByText(
-      'https://github.com/mitre/chef-canonical-ubuntu-16.04-lts-stig-hardening'
+    page.locator(
+      '[href*="https://github.com/mitre/chef-canonical-ubuntu-16.04-lts-stig-hardening"]'
     )
   ).toBeVisible();
   await expect(
-    page.getByText(
-      'https://github.com/mitre/canonical-ubuntu-16.04-lts-stig-baseline'
+    page.locator(
+      '[href*="https://github.com/mitre/canonical-ubuntu-16.04-lts-stig-baseline"]'
     )
   ).toBeVisible();
   await page.goto('/libs/validate');
+
+  await page.getByPlaceholder('Search for ...').click();
+  await page.getByPlaceholder('Search for ...').fill('windows');
 
   await page
     .getByRole('button', {name: 'View Details , Windows 2012 STIG'})
     .click();
   await expect(page).toHaveURL('/libs/guidance/windows-2012-stig?id=37');
   await expect(
-    page.getByRole('heading', {level: 3, name: 'Guidance Overview'})
+    page.getByRole('heading', {level: 1, name: 'Guidance Overview'})
   ).toBeVisible();
   await expect(
-    page.getByText('Windows Server 2012/2012 R2 Member Server')
+    page.getByText('Microsoft Windows 2012r2 Member Server STIG')
   ).toBeVisible();
   await expect(
-    page.getByText(
-      'https://github.com/mitre/chef-stig-windows-2012-v2r12-memberserver-hardening'
+    page.locator(
+      '[href*="https://github.com/mitre/microsoft-windows-2012r2-memberserver-stig-baseline"]'
     )
   ).toBeVisible();
-  await expect(
-    page.getByText(
-      'https://github.com/mitre/microsoft-windows-2012r2-memberserver-stig-baseline'
-    )
-  ).toBeVisible();
-  await page.goto('/libs/validate');
 });
